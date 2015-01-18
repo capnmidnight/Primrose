2a03e8d3f7ed06afbae48ed70326cf84a154a578
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2015-01-17 22:46:37 -0500

    site design

--- test/demo.js
+++ test/demo.js
@@ -5,14 +5,30 @@
  */
 
 function PrimroseDemo(w, h) {
-    var ctrls = findEverything();
-    prim = new Primrose("editor", {
-        width: w + "px",
-        height: h + "px",
-        mouseEventSource: ctrls.output,
-        file: PrimroseDemo.toString()
-    });
-    var scene = new THREE.Scene(),
+
+    function refreshSize() {
+        var w = ctrls.outputContainer.clientWidth,
+                h = ctrls.outputContainer.clientHeight;
+        renderer.setSize(w, h);
+        camera.aspect = w / h;
+        camera.updateProjectionMatrix();
+    }
+    
+    function render(t) {
+        requestAnimationFrame(render);
+        texture.needsUpdate = true;
+        cube.quaternion.setFromAxisAngle(rotAxis, t / 10000);
+        renderer.render(scene, camera);
+    }
+    
+    var ctrls = findEverything(),
+            prim = new Primrose("editor", {
+                width: w + "px",
+                height: h + "px",
+                mouseEventSource: ctrls.output,
+                file: PrimroseDemo.toString()
+            }),
+            scene = new THREE.Scene(),
             camera = new THREE.PerspectiveCamera(75, ctrls.output.width / ctrls.output.height, 0.1, 1000),
             renderer = new THREE.WebGLRenderer({
                 canvas: ctrls.output,
@@ -29,17 +45,17 @@ function PrimroseDemo(w, h) {
                 shading: THREE.FlatShading}),
             rotAxis = new THREE.Vector3(0.25, 1, 0.125);
 
-    cube = new THREE.Mesh(geometry, material);
-
-    renderer.setSize(ctrls.output.clientWidth, ctrls.output.clientHeight);
     texture.anisotropy = renderer.getMaxAnisotropy();
-
+    cube = new THREE.Mesh(geometry, material);
+    
     ctrls.controls.appendChild(prim.operatingSystemSelect);
     ctrls.controls.appendChild(prim.keyboardSelect);
     ctrls.controls.appendChild(prim.themeSelect);
-    
-    prim.placeSurrogateUnder(ctrls.output);
 
+    prim.placeSurrogateUnder(ctrls.output);
+    
+    window.addEventListener("resize", refreshSize);
+    refreshSize();
 
     // the following will be necessary for Three.js r70
     //renderer.setPixelRatio(window.devicePixelRatio);
@@ -47,11 +63,5 @@ function PrimroseDemo(w, h) {
     scene.add(cube);
     camera.position.z = 3;
     prim.focus();
-    function render(t) {
-        requestAnimationFrame(render);
-        texture.needsUpdate = true;
-        cube.quaternion.setFromAxisAngle(rotAxis, t / 10000);
-        renderer.render(scene, camera);
-    }
     requestAnimationFrame(render);
 }
