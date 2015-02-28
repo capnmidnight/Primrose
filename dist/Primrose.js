af801d7e844ea46a3cd69a2eb2ed1a0dd856f0ab
Sean T. McBeth <sean.mcbeth@gmail.com>
2015-02-28 10:05:14 -0500

    #30: plain text

--- dist/Primrose.js
+++ dist/Primrose.js
@@ -1,4 +1,4 @@
-/*! Primrose 2015-02-22
+/*! Primrose 2015-02-28
 Copyright (C) 2015 [object Object]
 https://github.com/capnmidnight/Primrose*/
 /* 
@@ -2749,6 +2749,25 @@ function isPointerLocked() {
 }
 var requestPointerLock = (document.documentElement.requestPointerLock || document.documentElement.webkitRequestPointerLock || document.documentElement.mozRequestPointerLock || function () {
 }).bind(document.documentElement);;/* 
+ * Copyright (C) 2015 Sean T. McBeth <sean@seanmcbeth.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+Grammar.PlainText = new Grammar("PlainText", [
+    ["newlines", /(?:\r\n|\r|\n)/]
+]);;/* 
  * Copyright (C) 2015 Sean T. McBeth <sean@seanmcbeth.com>
  *
  * This program is free software: you can redistribute it and/or modify
