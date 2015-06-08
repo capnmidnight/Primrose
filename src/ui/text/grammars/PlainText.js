dd00ee9452cb6bb8ffdda4e7ea5585509c45cc97
Sean T. McBeth <sean.mcbeth@gmail.com>
2015-06-08 14:48:10 -0400

    don't need as much hierarchy as I thought

--- src/ui/text/grammars/PlainText.js
+++
@@ -1,8 +0,0 @@
-/* global Primrose */
-Primrose.UI.Text.Grammars.PlainText = (function () {
-  "use strict";
-
-  return new Primrose.UI.Text.Grammar("PlainText", [
-    ["newlines", /(?:\r\n|\r|\n)/]
-  ]);
-})();
--- src/ui/text/operating_systems/OSX.js
+++
@@ -1,9 +0,0 @@
-/* global Primrose */
-Primrose.UI.Text.OperatingSystems.OSX = ( function () {
-  "use strict";
-
-  return new Primrose.UI.Text.OperatingSystem(
-      "OS X", "META", "ALT", "METASHIFT_z",
-      "META", "LEFTARROW", "RIGHTARROW",
-      "META", "UPARROW", "DOWNARROW" );
-} )();
