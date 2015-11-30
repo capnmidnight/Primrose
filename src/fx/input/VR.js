313bd0f710807ce682af19ebc7b14e9b051fcb14
Sean T. McBeth <sean.mcbeth@gmail.com>
2015-11-30 09:14:04 -0500

    need to guard these property accessor

--- src/fx/input/VR.js
+++ src/fx/input/VR.js
@@ -33,29 +33,43 @@ Primrose.Input.VR = ( function () {
   VRInput.prototype.update = function ( dt ) {
     if ( this.sensor ) {
       var state = this.sensor.getState();
+      
       if ( state.position ) {
         this.setAxis( "X", state.position.x );
         this.setAxis( "Y", state.position.y );
         this.setAxis( "Z", state.position.z );
       }
-      this.setAxis( "VX", state.linearVelocity.x );
-      this.setAxis( "VY", state.linearVelocity.y );
-      this.setAxis( "VZ", state.linearVelocity.z );
-      this.setAxis( "AX", state.linearAcceleration.x );
-      this.setAxis( "AY", state.linearAcceleration.y );
-      this.setAxis( "AZ", state.linearAcceleration.z );
+      
+      if(state.linearVelocity){
+        this.setAxis( "VX", state.linearVelocity.x );
+        this.setAxis( "VY", state.linearVelocity.y );
+        this.setAxis( "VZ", state.linearVelocity.z );
+      }
+      
+      if(state.linearAcceleration){
+        this.setAxis( "AX", state.linearAcceleration.x );
+        this.setAxis( "AY", state.linearAcceleration.y );
+        this.setAxis( "AZ", state.linearAcceleration.z );
+      }
+      
       if ( state.orientation ) {
         this.setAxis( "RX", state.orientation.x );
         this.setAxis( "RY", state.orientation.y );
         this.setAxis( "RZ", state.orientation.z );
         this.setAxis( "RW", state.orientation.w );
       }
-      this.setAxis( "RVX", state.angularVelocity.x );
-      this.setAxis( "RVY", state.angularVelocity.y );
-      this.setAxis( "RVZ", state.angularVelocity.z );
-      this.setAxis( "RAX", state.angularAcceleration.x );
-      this.setAxis( "RAY", state.angularAcceleration.y );
-      this.setAxis( "RAZ", state.angularAcceleration.z );
+      
+      if(state.angularVelocity){
+        this.setAxis( "RVX", state.angularVelocity.x );
+        this.setAxis( "RVY", state.angularVelocity.y );
+        this.setAxis( "RVZ", state.angularVelocity.z );
+      }
+      
+      if(state.angularAcceleration){
+        this.setAxis( "RAX", state.angularAcceleration.x );
+        this.setAxis( "RAY", state.angularAcceleration.y );
+        this.setAxis( "RAZ", state.angularAcceleration.z );
+      }
     }
     Primrose.Input.ButtonAndAxis.prototype.update.call( this, dt );
   };
