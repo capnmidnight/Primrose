4f170a51bbf0a9a7ff54a2ecd65a1041ee9f240e
Sean T. McBeth <sean.mcbeth@gmail.com>
2015-12-02 15:55:33 -0500

    build 0.16.8

--- bin/store.js
+++ bin/store.js
@@ -15,21 +15,20 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* global isMobile, StripeCheckout, gotVRDevices, PrimroseDemo, jQuery */
+/* global isMobile, StripeCheckout, gotVRDevices, Primrose, jQuery */
 
 window.fnames = [ 'EMAIL', 'FNAME', 'LNAME' ];
 window.ftypes = [ 'email', 'text', 'text' ];
 
 var ctrls,
-    prices,
-    lastTab;
+    lastTab = "home";
 
 function showSection ( id, skipPush ) {
   var tab;
-
   if ( id ) {
     tab = document.getElementById( id );
     if ( !tab ) {
+      // in case someone tries to type in their own hash
       tab = document.getElementsByTagName( "article" )[0];
       id = tab.id;
     }
@@ -43,128 +42,63 @@ function showSection ( id, skipPush ) {
       tab.className = "";
     }
     lastTab = lastTab === id ? null : id;
-    tabs = document.querySelectorAll( "#menu > a" );
+    tabs = document.querySelectorAll( "#menu > ul > li > a" );
     var r = new RegExp( fmt( "^javascript:showSection\\('($1)?'\\);$",
         lastTab ) );
     for ( i = 0; i < tabs.length; ++i ) {
       t = tabs[i];
       if ( r.test( t.href ) ) {
-        t.className = "selected button";
+        t.className = "primary selected button";
       }
       else {
-        t.className = "button";
+        t.className = "primary button";
       }
     }
+    
     var url = "#" + ( lastTab || "" );
     if ( !skipPush ) {
       history.pushState( null, "Primrose" + ( lastTab ? " > " + lastTab : "" ),
           url );
     }
 
-    ga( "send", "pageview", url );
+    //ga( "send", "pageview", url );
   }
 }
 
 window.addEventListener( "popstate", function ( evt ) {
-  var nextTab = window.location.hash;
+  var nextTab = location.hash;
   if ( nextTab.length > 0 ) {
     nextTab = nextTab.substring( 1 );
     showSection( nextTab, true );
   }
 } );
 
-function makeItemPurchase ( btn, lbl, stat, err, item ) {
-  var purchaseError = false;
-
-  function onPurchaseError ( btn, err, msg, ref ) {
-    var errMsg = document.getElementById( err.id + "-msg" );
-    var errRef = document.getElementById( err.id + "-ref" );
-    var errMail = document.getElementById( err.id + "-email" );
-
-    console.error( msg, ref || "", errMsg, errMail, errRef );
-
-    errMsg.innerHTML = msg;
-    errMail.innerHTML = "Sean T. McBeth &lt;sean@primroseeditor.com&gt;";
-    errMail.href = "mailto:sean@primroseeditor.com";
-    if ( ref ) {
-      errRef.innerHTML += fmt( "REF #: $1.", ref );
-      errMail.href += fmt( "?subject=Purchase Error REF #: $1", ref );
-    }
-
-    purchaseError = true;
-    btn.className = "disabled button";
-    err.style.display = "block";
-  }
-
-  function purchaseDone ( result ) {
-    if ( result.status === "ok" ) {
-      var tk = document.getElementById( stat.id + "-token" );
-      var lic = document.getElementById( stat.id + "-license" );
-
-      tk.innerHTML = result.id;
-      lic.innerHTML = result.license;
-
-      stat.style.display = "block";
-    }
-    else {
-      onPurchaseError( btn, err, fmt(
-          "The purchase process with Stripe failed. Reason: $1.",
-          result.error ), result.id );
-    }
-  }
-
-  if ( !prices || prices[item] === undefined ) {
-    btn.className = "disabled button";
-  }
-  else {
-    var priceTag = fmt( "$$1.00", prices.editor_license / 100 );
-
-    lbl.innerHTML = priceTag;
-
-    var stripeHandler = StripeCheckout.configure( {
-      key: prices.stripe_public_key,
-      image: 'images/logo128dark.png',
-      name: 'Primrose Editor',
-      zipCode: true,
-      token: function ( token ) {
-        token.item = item;
-        sendObject( document.location.protocol + "//" +
-            document.location.hostname + ":8082/purchase", token,
-            onPurchaseError.bind( this, btn, err,
-                "We couldn't communicate with our own server.", token.id ),
-            purchaseDone );
-      }
-    } );
-
-    window.addEventListener( 'popstate', stripeHandler.close.bind(
-        stripeHandler ) );
-    btn.addEventListener( "click", function () {
-      if ( !purchaseError ) {
-        stripeHandler.open( {
-          description: "Proprietary license (" + priceTag + ")",
-          amount: prices[item]
-        } );
-      }
-    } );
-  }
-}
-
 function initStore ( ) {
   ctrls = findEverything();
-  var versionLabels = document.getElementsByClassName( "version-label" );
-  for ( var i = 0; i < versionLabels.length; ++i ) {
-    versionLabels[i].innerHTML = Primrose.VERSION;
+  
+  function byClass(c, t){
+    Array.prototype.forEach.call(document.getElementsByClassName(c), t);
   }
-  if ( window.location.hash.length > 1 ) {
-    showSection( window.location.hash.substring( 1 ) );
+  
+  byClass( 
+    "version-label",
+    function(l){
+      l.innerHTML = Primrose.VERSION;
+    });
+  var pre = "bin/Primrose-" + Primrose.VERSION.substring(1);
+  byClass(
+    "download-link",
+    function(l){
+      l.href = pre + ".js";
+    });
+  byClass(
+    "download-minified-link",
+    function(l){
+      l.href = pre + ".min.js";
+    });
+  
+  var hash = location.hash.substring( 1 );
+  if ( hash.length > 0 && hash !== lastTab ) {
+    showSection( hash, true );
   }
-  else {
-    showSection( 'home' );
-  }
-
-  getObject( "data/config.json", function ( config ) {
-    prices = config;
-    makeItemPurchase( ctrls.buyLicenseButton, ctrls.licensePrice,
-        ctrls.purchaseSuccess, ctrls.purchaseError, "editor_license" );
-  } );
 }
