0d68652ec8d5398eba536e796e5a93856a4fe14d
Sean T. McBeth <sean.mcbeth@gmail.com>
2015-12-15 11:21:15 -0500

    Revert "deleting unused file"
    This reverts commit e990cdc258a51a59ddff9378b391b0107015bd83.

---
+++ src/fx/text/command_packs/TestViewer.js
@@ -0,0 +1,47 @@
+// If SHIFT is not held, then "front.
+// If SHIFT is held, then "back"
+/* global Primrose */
+Primrose.Text.CommandPacks.TextEditor = ( function () {
+  "use strict";
+
+  return {
+    name: "Basic commands",
+    NORMAL_SPACE: " ",
+    SHIFT_SPACE: " ",
+    NORMAL_BACKSPACE: function ( prim, tokenRows ) {
+      if ( prim.frontCursor.i === prim.backCursor.i ) {
+        prim.frontCursor.left( tokenRows );
+      }
+      prim.overwriteText();
+      prim.scrollIntoView( prim.frontCursor );
+    },
+    NORMAL_ENTER: function ( prim, tokenRows, currentToken ) {
+      var indent = "";
+      var tokenRow = tokenRows[prim.frontCursor.y];
+      if ( tokenRow.length > 0 && tokenRow[0].type === "whitespace" ) {
+        indent = tokenRow[0].value;
+      }
+      prim.overwriteText( "\n" + indent );
+      prim.scrollIntoView( prim.frontCursor );
+    },
+    NORMAL_DELETE: function ( prim, tokenRows ) {
+      if ( prim.frontCursor.i === prim.backCursor.i ) {
+        prim.backCursor.right( tokenRows );
+      }
+      prim.overwriteText();
+      prim.scrollIntoView( prim.frontCursor );
+    },
+    SHIFT_DELETE: function ( prim, tokenRows ) {
+      if ( prim.frontCursor.i === prim.backCursor.i ) {
+        prim.frontCursor.home( tokenRows );
+        prim.backCursor.end( tokenRows );
+      }
+      prim.overwriteText();
+      prim.scrollIntoView( prim.frontCursor );
+    },
+    NORMAL_TAB: function ( prim, tokenRows ) {
+      var ts = prim.getTabString();
+      prim.overwriteText( ts );
+    }
+  };
+} )();
