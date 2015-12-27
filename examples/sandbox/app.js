c0f01f6649a3a4a9e8da9a15ec19f4c74f7fba26
Sean T. McBeth <sean.mcbeth@gmail.com>
2015-12-26 20:25:57 -0500

    bleh

--- examples/sandbox/app.js
+++ examples/sandbox/app.js
@@ -2,10 +2,16 @@
 function StartDemo () {
 
   var app = new Primrose.VRApplication(
-      "sandbox",
-      {
+      "sandbox", {
+        // This file references a THREE.js JSON-formatted scene
+        // file, which basically describes a room or level in 
+        // which the user will walk around, on which we can 
+        // create additional objects.
         sceneModel: "../models/scene3.json",
         button: {
+          // This file references another THREE.js JSON-formatted
+          // model file, which will be cloned anytime a user
+          // calls createElement("button").
           model: "../models/smallbutton.json",
           options: {
             colorUnpressed: 0x7f0000,
@@ -15,11 +21,11 @@ function StartDemo () {
       } );
 
   app.addEventListener( "ready", function () {
-    var n = 8;
-    var d = ( n - 1 ) / 2;
+    var n = 8,
+        d = ( n - 1 ) / 2;
     for ( var i = 0; i < n; ++i ) {
-      var btn = app.createElement( "button" );
-      var x = ( i - d ) * 0.25;
+      var btn = app.createElement( "button" ),
+          x = ( i - d ) * 0.25;
       btn.moveBy( x, 0, -1.5 * Math.cos( x ) + 1 );
       btn.addEventListener(
           "click",
