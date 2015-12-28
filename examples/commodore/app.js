f4e7b1eebf811aff918fa0e5faedef4c7a86a6fc
Sean T. McBeth <sean.mcbeth@gmail.com>
2015-12-28 12:26:12 -0500

    more getting the main site ready to transition to the new design

--- examples/commodore/app.js
+++ examples/commodore/app.js
@@ -1,75 +1,71 @@
 /* global isOSX, Primrose, THREE, isMobile, requestFullScreen, put */
-console.clear();
 
-var app;
-function PrimroseDemo () {
-  var keyState = {},
-      modA = isOSX ? "metaKey" : "ctrlKey",
-      modB = isOSX ? "altKey" : "shiftKey",
-      execKey = isOSX ? "E" : "SPACE",
-      terminal = null,
-      app = new Primrose.VRApplication( "Commodore", {
-        sceneModel: "commodore_pet.json",
-        skyTexture: "../images/bg2.jpg",
-        groundTexture: "../images/deck.png"
-      } );
+var keyState = {},
+    modA = isOSX ? "metaKey" : "ctrlKey",
+    modB = isOSX ? "altKey" : "shiftKey",
+    execKey = isOSX ? "E" : "SPACE",
+    terminal = null,
+    app = new Primrose.VRApplication( "Commodore", {
+      sceneModel: "commodore_pet.json",
+      skyTexture: "../images/bg2.jpg",
+      groundTexture: "../images/deck.png"
+    } );
 
 
-  app.ctrls.goVR.addEventListener( "click", app.goFullScreen.bind( app, false ), false );
-  app.ctrls.goRegular.addEventListener( "click", app.goFullScreen.bind( app, true ), false );
+app.ctrls.goVR.addEventListener( "click", app.goFullScreen.bind( app, false ), false );
+app.ctrls.goRegular.addEventListener( "click", app.goFullScreen.bind( app, true ), false );
 
-  function connectVR ( ) {
-    if ( app.input.vr.deviceIDs.length > 0 ) {
-      if ( app.ctrls.goVR ) {
-        app.ctrls.goVR.style.display = "inline-block";
-      }
-    }
-    else if ( app.ctrls.goVR ) {
-      app.ctrls.goVR.style.display = "none";
+function connectVR ( ) {
+  if ( app.input.vr.deviceIDs.length > 0 ) {
+    if ( app.ctrls.goVR ) {
+      app.ctrls.goVR.style.display = "inline-block";
     }
   }
-
-  app.input.addEventListener( "vrdeviceconnected", connectVR, false );
-  app.input.addEventListener( "vrdevicelost", connectVR, false );
-
-  window.addEventListener( "keydown", keyDown.bind( this ) );
-  window.addEventListener( "keyup", function ( evt ) {
-    keyState[evt.keyCode] = false;
-  } );
-
-  function keyDown ( evt ) {
-    if ( !this.currentEditor ||
-        !this.currentEditor.focused ) {
-      keyState[evt.keyCode] = true;
-    }
-    else if ( terminal.running &&
-        terminal.waitingForInput &&
-        evt.keyCode === Primrose.Keys.ENTER ) {
-      terminal.sendInput( evt );
-    }
-    else if ( !terminal.running &&
-        isExecuteCommand( evt ) ) {
-      terminal.execute();
-    }
+  else if ( app.ctrls.goVR ) {
+    app.ctrls.goVR.style.display = "none";
   }
-
-  function isExecuteCommand ( evt ) {
-    return evt[modA] && evt[modB] && evt.keyCode === Primrose.Keys[execKey];
-  }
-
-  app.addEventListener( "ready", function () {
-
-    this.scene.traverse( function ( obj ) {
-      if ( obj.name && obj.parent && obj.parent.uuid === this.scene.uuid ) {
-        obj.position.y += this.avatarHeight * 0.9;
-        obj.position.z -= 1;
-      }
-    }.bind( this ) );
-
-    put( light( 0xffffff ) ).on( this.scene ).at( 0, 20, 0 );
-
-    this.convertToEditor( this.scene.Screen );
-    terminal = new Primrose.Text.Terminal( this.scene.Screen.textarea );
-    terminal.loadFile( "../oregon.bas" );
-  }.bind( app ) );
 }
+
+app.input.addEventListener( "vrdeviceconnected", connectVR, false );
+app.input.addEventListener( "vrdevicelost", connectVR, false );
+
+window.addEventListener( "keydown", keyDown.bind( this ) );
+window.addEventListener( "keyup", function ( evt ) {
+  keyState[evt.keyCode] = false;
+} );
+
+function keyDown ( evt ) {
+  if ( !this.currentEditor ||
+      !this.currentEditor.focused ) {
+    keyState[evt.keyCode] = true;
+  }
+  else if ( terminal.running &&
+      terminal.waitingForInput &&
+      evt.keyCode === Primrose.Keys.ENTER ) {
+    terminal.sendInput( evt );
+  }
+  else if ( !terminal.running &&
+      isExecuteCommand( evt ) ) {
+    terminal.execute();
+  }
+}
+
+function isExecuteCommand ( evt ) {
+  return evt[modA] && evt[modB] && evt.keyCode === Primrose.Keys[execKey];
+}
+
+app.addEventListener( "ready", function () {
+
+  this.scene.traverse( function ( obj ) {
+    if ( obj.name && obj.parent && obj.parent.uuid === this.scene.uuid ) {
+      obj.position.y += this.avatarHeight * 0.9;
+      obj.position.z -= 1;
+    }
+  }.bind( this ) );
+
+  put( light( 0xffffff ) ).on( this.scene ).at( 0, 20, 0 );
+
+  this.convertToEditor( this.scene.Screen );
+  terminal = new Primrose.Text.Terminal( this.scene.Screen.textarea );
+  terminal.loadFile( "../oregon.bas" );
+}.bind( app ) );
---
+++ examples/music/app.js
@@ -0,0 +1,70 @@
+var app = new Primrose.VRApplication(
+    // We give the application a name here, in case we ever
+    // hook it up to the Multiplayer Server.
+    "sandbox", {
+      // This file references a THREE.js JSON-formatted scene
+      // file, which basically describes a room or level in 
+      // which the user will walk around, on which we can 
+      // create additional objects.
+      sceneModel: "../models/holodeck.json",
+      // One of our GUI controls is configured here.
+      button: {
+        // This file references another THREE.js JSON-formatted
+        // model file, which will be cloned anytime a user
+        // calls createElement("button").
+        model: "../models/smallbutton.json",
+        // Display settings for different states of the button.
+        options: {
+          colorUnpressed: 0x7f0000,
+          colorPressed: 0x007f00
+        }
+      }
+    } );
+
+// Once Primrose has setup the WebGL context, setup THREE.js, 
+// downloaded and validated all of model files, and constructed
+// the basic scene hierarchy out of it, the "ready" event is fired,
+// indicating that we may make additional changes to the scene now.
+app.addEventListener( "ready", function () {
+
+  var numButtons = 8,
+      middle = ( numButtons - 1 ) / 2;
+  for ( var i = 0; i < numButtons; ++i ) {
+
+    // Primrose uses an object model that is layered on top of
+    // DOM, to get users up to speed quickly and to integrate
+    // easily with other tools.
+    var btn = document.createElement( "button" );
+    btn.type = "button";
+
+    // Spacing the buttons out in a rough arc around the origin.
+    var x = ( i - middle ) * 0.25,
+        z = -1.5 * Math.cos( x ) + 1;
+
+    // Though this is the CSS transform syntax, the only units that
+    // are supported at this time are Ems, and they are treated as
+    // meters.
+    btn.style.transform = "translate3d(" + x + "em, 0, " + z + "em)";
+
+    // We can wire up event handlers on the button just like it was
+    // a DOM element.
+    btn.addEventListener(
+        "click",
+        // Primrose already has a basic sound API setup, so you
+        // can hook directly into it to create basic, procedurally
+        // generated music.
+        app.music.play.bind(
+            app.music,
+            // The play method understands Piano key indices, so
+            // we just need to calculate the note we want to play,
+            // understanding Middle C to be note 44.
+            35 + i * 4,
+            // Volume, on the range [0, 1]. Full volume is quite loud.
+            0.30,
+            // Duration to play, in seconds.
+            0.2 ) );
+
+    // Put the element into the scene
+    app.appendChild( btn );
+  }
+} );
