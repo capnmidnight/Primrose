066bc0aa52e8464469687d6554260ff7d504c2d4
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-01-05 10:56:15 -0500

    dedupe code, better type description

--- src/core.js
+++ src/core.js
@@ -37,9 +37,17 @@ if ( typeof window.pliny === "undefined" ) {
   } )();
 }
 
-pliny.the.elder.namespace( "Primrose", {description: "Primrose is a framework for making applications in WebVR."} );
-pliny.the.elder.namespace( "Primrose.Input", {description: "The Input namespace contains classes that handle user input, for use in navigating the 3D environment."} );
-pliny.the.elder.namespace( "Primrose.Output", {description: "The Output namespace contains classes that handle output to devices other than the screen (e.g. Audio, Music, etc.)."} );
+pliny.the.elder.namespace( "Primrose", "Primrose is a framework for making applications in WebVR. The top-level namespace contains classes for manipulating and viewing 3D environments." );
+pliny.the.elder.namespace( "Primrose.Input", "The Input namespace contains classes that handle user input, for use in navigating the 3D environment." );
+pliny.the.elder.namespace( "Primrose.Output", "The Output namespace contains classes that handle output to devices other than the screen (e.g. Audio, Music, etc.)." );
+pliny.the.elder.namespace( "Primrose.Text", "The Text namespace contains classes everything regarding the Primrose source code editor." );
+pliny.the.elder.namespace( "Primrose.Text.CodePages", "The CodePages namespace contains international keyboard parameters." );
+pliny.the.elder.namespace( "Primrose.Text.CommandPacks", "The CodePages namespace contains sets of keyboard shortcuts for different types of text-oriented controls." );
+pliny.the.elder.namespace( "Primrose.Text.Controls", "The CodePages namespace contains different types of text-oriented controls." );
+pliny.the.elder.namespace( "Primrose.Text.Grammars", "The CodePages namespace contains grammar parsers for different types of programming languages, to enable syntax highlighting." );
+pliny.the.elder.namespace( "Primrose.Text.OperatingSystems", "The CodePages namespace contains sets of keyboard shortcuts for different operating systems." );
+pliny.the.elder.namespace( "Primrose.Text.Renderers", "The CodePages namespace contains different renderers for using the general Text Editor logic in different output systems. Current, Canvas2D is the only system that works. A system for DOM elements exists, but it is broken and not likely to be fixed any time soon." );
+pliny.the.elder.namespace( "Primrose.Text.CodePages", "The CodePages namespace contains color themes for text-oriented controls, for use when coupled with a parsing grammar." );
 var Primrose = {
   Input: {},
   Output: {},
@@ -60,3 +68,8 @@ var Primrose = {
     "#CB8442", "#BD723C", "#704139", "#A3866A", "#870400", "#710101", "#430000",
     "#5B0001", "#302E2E" ]
 };
+pliny.the.elder.value( "Primrose.SYS_FONTS", "A selection of fonts that will match whatever the user's operating system normally uses." );
+pliny.the.elder.value( "Primrose.SKINS", {
+  type: "Array of String",
+  description: "A selectioin of color values that closely match skin colors of people."
+});
