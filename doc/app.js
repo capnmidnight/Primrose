2a6e0824caa1233926aae22f51e064c490ea50ba
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-01-07 13:12:42 -0500

    be able to export raw pliny documentation objects

--- doc/app.js
+++ doc/app.js
@@ -1,12 +1,26 @@
 /*global pliny, Primrose*/
 var oldConsole = console.log;
-console.log = function(output){
+console.log = function ( output ) {
   document.body.innerHTML += "<pre>" + output + "</pre>";
 };
 
+function recurse ( name, obj, stack, subName ) {
+  var arr = obj[subName];
+  if ( arr && arr instanceof Array ) {
+    for ( var i = 0; i < arr.length; ++i ) {
+      stack.push( name + "." + arr[i].name );
+    }
+  }
+}
 
-Primrose.help();
-Primrose.Button.help();
+function walk ( name ) {
+  var stack = [ name ];
+  while ( stack.length > 0 ) {
+    name = stack.shift();
+    pliny.theYounger( name );
+    [ "namespaces", "functions", "classes", "methods" ]
+        .forEach( recurse.bind( null, name, pliny.get( name ), stack ) );
+  }
+}
 
-
-console.log = oldConsole;
+walk( "Primrose" );
