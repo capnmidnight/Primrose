6dd0ee14933af32e949da744c73340538152b2a1
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-01-09 11:10:44 -0500

    better module init, and a distinct database object, for pliny

--- doc/app.js
+++ doc/app.js
@@ -4,38 +4,26 @@
 
 var log = console.log.bind( console );
 console.log = function ( output ) {
-  document.body.innerHTML += "<p>" + output.replace( /\t/g, "&nbsp;&nbsp;&nbsp;&nbsp;" ).split( "\n" ).join( "<br>" ) + "</p>";
+  document.body.innerHTML += "<p>" + output.replace( /\t/g, "&nbsp;&nbsp;&nbsp;&nbsp;" ).split( "\n" ).join( "<br>" ) + "</p><hr>";
 };
 
-function recurse ( name, obj, stack, subName ) {
-  var arr = obj[subName];
-  if ( arr && arr instanceof Array ) {
-    for ( var i = 0; i < arr.length; ++i ) {
-      if ( name.length > 0 ) {
-        stack.push( name + "." + arr[i].name );
-      }
-      else {
-        stack.push( arr[i].name );
+function recurse ( obj, stack, subName ) {
+  if ( obj ) {
+    var arr = obj[subName];
+    if ( arr && arr instanceof Array ) {
+      for ( var i = 0; i < arr.length; ++i ) {
+        stack.push( arr[i] );
       }
     }
   }
 }
 
-var stack = pliny.values.slice();
-stack.push( "Primrose" );
+var stack = [ pliny.database ];
 while ( stack.length > 0 ) {
-  var obj = stack.shift(),
-      bag;
-  if ( typeof obj === "string" || obj instanceof String ) {
-    bag = pliny.get( obj );
-  }
-  else {
-    bag = obj;
-    obj = bag.name || "";
-  }
+  var obj = stack.shift();
   pliny.theYounger( obj );
   [ "namespaces", "functions", "classes", "methods", "enumerations", "records" ]
-      .forEach( recurse.bind( null, obj, bag, stack ) );
+      .forEach( recurse.bind( null, obj, stack ) );
 }
 
 
