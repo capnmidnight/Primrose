73002b147c0280475b0eb8fe4dac6aa46f69b50a
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-01-11 13:12:45 -0500

    documentation layout updates for better mobile support

--- doc/app.js
+++ doc/app.js
@@ -1,70 +1,83 @@
 /* global pliny, Primrose */
+( function () {
+  "use strict";
 
-"use strict";
+  var docSearch = document.getElementById( "docSearch" ),
+      main = document.querySelector( "main" ),
+      nav = document.querySelector( "#contents nav > ul" ),
+      doc = document.querySelector( "#documentation" ),
+      header = document.querySelector( "header" ),
+      docoCache = {"": doc.innerHTML},
+  groupings = {
+    examples: [ ],
+    namespaces: [ ],
+    classes: [ ],
+    functions: [ ],
+    enumerations: [ ],
+    records: [ ]
+  };
 
-var docSearch = document.getElementById( "docSearch" ),
-    nav = document.querySelector( "#contents > nav > ul" ),
-    main = document.querySelector( "#documentation" ),
-    docoCache = {"": main.innerHTML},
-groupings = {
-  examples: [ ],
-  namespaces: [ ],
-  classes: [ ],
-  functions: [ ],
-  enumerations: [ ],
-  records: [ ]
-};
-
-function search () {
-  var elems = document.querySelectorAll( "#contents li li" ),
-      search = this.value.toLocaleLowerCase();
-  for ( var i = 0; i < elems.length; ++i ) {
-    var e = elems[i],
-        b = e.dataset.name.toLocaleLowerCase();
-    console.log( b, search, b.indexOf( search ) );
-    e.style.display = ( search.length === 0 || b.indexOf( search ) > -1 ) ? "" : "none";
+  function search () {
+    var elems = document.querySelectorAll( "#contents li li" ),
+        search = this.value.toLocaleLowerCase();
+    for ( var i = 0; i < elems.length; ++i ) {
+      var e = elems[i],
+          b = e.dataset.name.toLocaleLowerCase();
+      e.style.display = ( search.length === 0 || b.indexOf( search ) > -1 ) ? "" : "none";
+    }
   }
-}
 
-function showHash ( evt ) {
-  main.innerHTML = docoCache[document.location.hash] || ( "Not found: " + document.location.hash );
-  if ( evt ) {
-    main.parentElement.scrollTo( 0, 0 );
-  }
-}
-
-docSearch.addEventListener( "keyup", search, false );
-docSearch.addEventListener( "change", search, false );
-
-var stack = [ pliny.database ];
-while ( stack.length > 0 ) {
-  var obj = stack.shift();
-  for ( var key in obj ) {
-    if ( groupings[key] ) {
-      var collect = obj[key],
-          group = groupings[key];
-      for ( var i = 0; i < collect.length; ++i ) {
-        group.push( collect[i] );
-        stack.push( collect[i] );
+  function showHash ( evt ) {
+    if ( document.location.hash !== "#top" ) {
+      doc.innerHTML = docoCache[document.location.hash] || ( "Not found: " + document.location.hash );
+      if ( evt ) {
+        doc.scrollIntoView( {
+          block: "top",
+          behavior: "smooth"
+        } );
       }
     }
   }
-}
 
-var output = "";
-for ( var g in groupings ) {
-  var group = groupings[g];
-  output += "<li><h2>" + g + "</h2><ul>";
-  for ( var i = 0; i < group.length; ++i ) {
-    var obj = group[i];
-    output += "<li data-name=\"" + obj.fullName + "\"><a href=\"#" + obj.id + "\">" + obj.fullName + "</a></li>";
-    docoCache["#" + obj.id] = pliny.formats.html.format( obj );
+  function resize () {
+    main.style.top = ( header.clientHeight + 1 ) + "px";
   }
-  output += "</ul></li>";
-}
 
-nav.innerHTML += output;
+  var stack = [ pliny.database ];
+  while ( stack.length > 0 ) {
+    var obj = stack.shift();
+    for ( var key in obj ) {
+      if ( groupings[key] ) {
+        var collect = obj[key],
+            group = groupings[key];
+        for ( var i = 0; i < collect.length; ++i ) {
+          group.push( collect[i] );
+          stack.push( collect[i] );
+        }
+      }
+    }
+  }
 
-window.addEventListener( "hashchange", showHash, false );
+  var output = "";
+  for ( var g in groupings ) {
+    var group = groupings[g];
+    output += "<li><h2>" + g + "</h2><ul>";
+    for ( var i = 0; i < group.length; ++i ) {
+      var obj = group[i];
+      output += "<li data-name=\"" + obj.fullName + "\"><a href=\"#" + obj.id + "\">" + obj.fullName + "</a></li>";
+      docoCache["#" + obj.id] = pliny.formats.html.format( obj ) + "<a class=\"return-to-top\" href=\"#top\">top</a>";
+    }
+    output += "</ul></li>";
+  }
 
-showHash();
+  nav.innerHTML += output;
+
+
+  docSearch.addEventListener( "keyup", search, false );
+  docSearch.addEventListener( "change", search, false );
+  window.addEventListener( "resize", resize, false );
+  window.addEventListener( "hashchange", showHash, false );
+
+  resize();
+  showHash();
+} )();
