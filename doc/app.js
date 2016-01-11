5a9f8552595151c987f130ab16bd7f6ceef2c90c
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-01-10 23:12:12 -0500

    basic documentation design and search

--- doc/app.js
+++ doc/app.js
@@ -2,6 +2,21 @@
 
 "use strict";
 
+var t = document.getElementById( "docSearch" );
+t.addEventListener( "keyup", search, false );
+t.addEventListener( "change", search, false );
+
+function search () {
+  var elems = document.querySelectorAll("#contents li li"),
+      search = this.value.toLocaleLowerCase();
+  for(var i = 0; i < elems.length; ++i){
+    var e = elems[i],
+        b = e.dataset.name.toLocaleLowerCase();
+    console.log(b, search, b.indexOf(search));
+    e.style.display = (search.length === 0 || b.indexOf(search) > -1) ? "" : "none";
+  }
+}
+
 function recurse ( obj, stack, namespaces, subName ) {
   if ( obj ) {
     var arr = obj[subName];
@@ -17,7 +32,7 @@ function recurse ( obj, stack, namespaces, subName ) {
 }
 
 var stack = [ pliny.database ],
-    nav = document.querySelector( "nav" ),
+    nav = document.querySelector( "#contents > nav > ul" ),
     main = document.querySelector( "main" ),
     namespaces = [ pliny.database ];
 
@@ -34,24 +49,24 @@ while ( stack.length > 0 ) {
       .forEach( recurse.bind( null, obj, stack, namespaces ) );
 }
 
-var output = "<ul>";
+var output = "";
 for ( var i = 0; i < namespaces.length; ++i ) {
   var ns = namespaces[i];
   output += "<li><h2><a href=\"#" + ns.id + "\">" + ns.fullName + "</a></h2>";
   [ "functions",
     "classes",
     "enumerations",
-    "records"]
+    "records" ]
       .forEach( function ( sub ) {
         var arr = ns[sub];
         if ( arr ) {
           output += "<h3>" + sub + "</h3><ul>";
           for ( var i = 0; i < arr.length; ++i ) {
-            output += "<li><a href=\"#" + arr[i].id + "\">" + arr[i].name + "</a></li>";
+            output += "<li data-name=\"" + arr[i].fullName + "\"><a href=\"#" + arr[i].id + "\">" + arr[i].name + "</a></li>";
           }
           output += "</ul>";
         }
       } );
   output += "</li>";
 }
-nav.innerHTML += output + "</ul>";
+nav.innerHTML += output;
