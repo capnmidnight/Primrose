1bf383bb166a0ce3b9de5371cd31eb5de7c956f7
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-01-13 21:45:11 -0500

    manually format file

--- src/fx/Angle.js
+++ src/fx/Angle.js
@@ -1,8 +1,8 @@
 /* global Primrose, pliny */
 
 Primrose.Angle = ( function ( ) {
-pliny.theElder.class( "Primrose", {
-name: "Angle",
+  pliny.theElder.class( "Primrose", {
+    name: "Angle",
     description: "The Angle class smooths out the jump from 360 to 0 degrees. It keeps track of the previous state of angle values and keeps the change between angle values to a maximum magnitude of 180 degrees, plus or minus. This allows for smoother opperation as rotating past 360 degrees will not reset to 0, but continue to 361 degrees and beyond, while rotating behind 0 degrees will not reset to 360 but continue to -1 and below.\n\
 \n\
 When instantiating, choose a value that is as close as you can guess will be your initial sensor readings.\n\
@@ -12,14 +12,14 @@ This is particularly important for the 180 degrees, +- 10 degrees or so. If you 
 It also automatically performs degree-to-radian and radian-to-degree conversions.\n\
 ![Radians](https://upload.wikimedia.org/wikipedia/commons/4/4e/Circle_radians.gif)",
     parameters: [
-    {name: "initialAngleInDegrees", type: "Number", description: "(Required) Specifies the initial context of the angle. Zero is not always the correct value."}
+      {name: "initialAngleInDegrees", type: "Number", description: "(Required) Specifies the initial context of the angle. Zero is not always the correct value."}
     ],
     references: [
-    {name: "Radian - Wikipedia, the free encyclopedia.", description: "https://en.wikipedia.org/wiki/Radian"}
+      {name: "Radian - Wikipedia, the free encyclopedia.", description: "https://en.wikipedia.org/wiki/Radian"}
     ],
     examples: [ {
-    name: "Basic usage",
-        description: "To use the Angle class, create an instance of it with `new`, and modify the `degrees` property.\n\
+      name: "Basic usage",
+      description: "To use the Angle class, create an instance of it with `new`, and modify the `degrees` property.\n\
 \n\
 Code:\n\
 ``var a = new Primrose.Angle(356);\n\
@@ -27,9 +27,9 @@ a.degrees += 5;\n\
 console.log(a.degrees);``\n\
 Results:\n\
 ``361``"
-    }, {
-    name: "Convert degrees to radians",
-        description: "Create an instance of of Primrose.Angle, modify the `degrees` property, and read the `radians` property.\n\
+      }, {
+      name: "Convert degrees to radians",
+      description: "Create an instance of Primrose.Angle, modify the `degrees` property, and read the `radians` property.\n\
 \n\
 Code:\n\
 ``var a = new Primrose.Angle(10);\n\
@@ -37,9 +37,9 @@ a.degrees += 355;\n\
 console.log(a.radians);``\n\
 Results:\n\
 ``0.08726646259971647``"
-    }, {
-    name: "Convert radians to degress",
-        description: "Create an instance of of Primrose.Angle, modify the `radians` property, and read the `degrees` property.\n\
+      }, {
+      name: "Convert radians to degress",
+      description: "Create an instance of Primrose.Angle, modify the `radians` property, and read the `degrees` property.\n\
 \n\
 Code:\n\
 ``var a = new Primrose.Angle(0);\n\
@@ -47,13 +47,12 @@ a.radians += Math.PI / 2;\n\
 console.log(a.degrees);``\n\
 Results:\n\
 ``90``"
-    }
+      }
     ]
-} );
-    function Angle ( v ) {
+  } );
+  function Angle ( v ) {
     if ( typeof ( v ) !== "number" ) {
-    throw new Error(
-        "Angle must be initialized with a number. Initial value was: " + v );
+      throw new Error("Angle must be initialized with a number. Initial value was: " + v );
     }
 
     var value = v,
@@ -63,47 +62,50 @@ Results:\n\
         d3,
         DEG2RAD = Math.PI / 180,
         RAD2DEG = 180 / Math.PI;
-        pliny.theElder.property( {
-        name: "degrees",
-            type: "Number",
-            description: "Get/set the current value of the angle in degrees."} );
-        Object.defineProperty( this, "degrees", {
-        set: function ( newValue ) {
-        do {
-        // figure out if it is adding the raw value, or whole
-        // rotations of the value, that results in a smaller
-        // magnitude of change.
-        d1 = newValue + delta - value;
-            d2 = Math.abs( d1 + 360 );
-            d3 = Math.abs( d1 - 360 );
-            d1 = Math.abs( d1 );
-            if ( d2 < d1 && d2 < d3 ) {
-        delta += 360;
-        }
-        else if ( d3 < d1 ) {
-        delta -= 360;
-        }
-        } while ( d1 > d2 || d1 > d3 );
-            value = newValue + delta;
-        },
-            get: function ( ) {
-            return value;
-            }
-        } );
-        pliny.theElder.property( {
-        name: "radians",
-            type: "Number",
-            description: "Get/set the current value of the angle in radians."
-        } );
-        Object.defineProperty( this, "radians", {
-        get: function ( ) {
-        return this.degrees * DEG2RAD;
-        },
-            set: function ( val ) {
-            this.degrees = val * RAD2DEG;
-            }
-        } );
-    }
 
-return Angle;
-    } )( );
+    pliny.theElder.property( {
+      name: "degrees",
+      type: "Number",
+      description: "Get/set the current value of the angle in degrees."
+    } );
+    Object.defineProperty( this, "degrees", {
+      set: function ( newValue ) {
+        do {
+          // figure out if it is adding the raw value, or whole
+          // rotations of the value, that results in a smaller
+          // magnitude of change.
+          d1 = newValue + delta - value;
+          d2 = Math.abs( d1 + 360 );
+          d3 = Math.abs( d1 - 360 );
+          d1 = Math.abs( d1 );
+          if ( d2 < d1 && d2 < d3 ) {
+            delta += 360;
+          }
+          else if ( d3 < d1 ) {
+            delta -= 360;
+          }
+        } while ( d1 > d2 || d1 > d3 );
+        value = newValue + delta;
+      },
+      get: function ( ) {
+        return value;
+      }
+    } );
+
+    pliny.theElder.property( {
+      name: "radians",
+      type: "Number",
+      description: "Get/set the current value of the angle in radians."
+    } );
+    Object.defineProperty( this, "radians", {
+      get: function ( ) {
+        return this.degrees * DEG2RAD;
+      },
+      set: function ( val ) {
+        this.degrees = val * RAD2DEG;
+      }
+    } );
+  }
+  
+  return Angle;
+} )( );
