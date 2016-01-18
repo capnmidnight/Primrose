8f454f46f49469416bc92a65da9c72bce573d72d
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-01-17 22:30:16 -0500

    intro documentation update

--- src/fx/DOM.js
+++ src/fx/DOM.js
@@ -8,7 +8,13 @@ Primrose.DOM = ( function () {
 
   pliny.theElder.function( "Primrose.DOM", {
     name: "cascadeElement",
-    description: "1) If id is a string, tries to find the DOM element that has said ID\n      a) if it exists, and it matches the expected tag type, returns the\n          element, or throws an error if validation fails.\n      b) if it doesn't exist, creates it and sets its ID to the provided\n          id, then returns the new DOM element, not yet placed in the\n          document anywhere.\n2) If id is a DOM element, validates that it is of the expected type,\n      a) returning the DOM element back if it's good,\n      b) or throwing an error if it is not\n3) If id is null, creates the DOM element to match the expected type.",
+    description: "1. If id is a string, tries to find the DOM element that has said ID\n\
+* If it exists, and it matches the expected tag type, returns the element, or throws an error if validation fails.\n\
+* If it doesn't exist, creates it and sets its ID to the provided id, then returns the new DOM element, not yet placed in the document anywhere.\n\
+2. If id is a DOM element, validates that it is of the expected type,\n\
+* returning the DOM element back if it's good,\n\
+* or throwing an error if it is not\n\
+3. If id is null, creates the DOM element to match the expected type.",
     parameters: [
       {name: "id", type: "(String|Element)", description: "A vague reference to the element. Either a String id where the element can be had, a String id to give a newly created element if it does not exist, or an Element to manipulate and validate"},
       {name: "tag", type: "String", description: "The HTML tag name of the element we are finding/creating/validating."},
