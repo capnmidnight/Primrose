60c4d3cd6b049c33929da4e9428b8c72ae7625e1
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-01-18 09:58:20 -0500

    make a file out of the resizer, so it can later be minified.

--- examples/test/app.js
+++ examples/test/app.js
@@ -4,20 +4,8 @@ function println () {
   output.appendChild( document.createTextNode( fmt.apply( this, arguments ) + "\n" ) );
 }
 
-function walk ( d ) {
-  for ( var k in d ) {
-    var s = k.toLowerCase();
-    if ( k.indexOf( "full" ) > -1 && k.indexOf( "screen" ) > -1 ) {
-      println( k );
-      println( d[k] );
-    }
-  }
+function resize () {
+  println( "$5: $1 x $2 -> $3 x $4", screen.width, screen.height, window.innerWidth, window.innerHeight, window.orientation );
 }
-
-println("window");
-walk(window);
-println("document");
-walk(document);
-println("documentElement");
-walk(document.documentElement);
-println(screen.width + "x" + screen.height);
+window.addEventListener( "resize", resize );
+resize();
