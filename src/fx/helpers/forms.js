43f6078d23b1ceca38cb6cfb5cba7711a6462eaa
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-01-26 17:25:39 -0500

    documented readForm

--- src/fx/helpers/forms.js
+++ src/fx/helpers/forms.js
@@ -99,7 +99,7 @@ setSetting(\"A\", \"modified-A\");\n\
 console.assert(getSetting(\"A\", \"default-A\") === \"modified-A\");\n\
 deleteSetting(\"A\");\n\
 console.assert(getSetting(\"A\", \"default-A\") === \"default-A\");``"
-  }]
+    } ]
 } );
 function deleteSetting ( name ) {
   if ( window.localStorage ) {
@@ -109,15 +109,29 @@ function deleteSetting ( name ) {
 
 pliny.issue( "", {
   name: "document readForm",
-  type: "open",
+  type: "closed",
   description: "Finish writing the documentation for the `readForm()` function in the helpers/forms.js file."
 } );
 pliny.function( "", {
   name: "readForm",
-  parameters: [ ],
+  parameters: [
+    {name: "ctrls", type: "Array of Elements", description: "An array of HTML form elements, aka INPUT, TEXTAREA, SELECT, etc."}
+  ],
   returns: "Object",
-  description: "",
-  examples: [ ]
+  description: "Scans through an array of input elements and builds a state object that contains the values the input elements represent. Elements that do not have an ID attribute set, or have an attribute `data-skipcache` set, will not be included.",
+  examples: [ {
+      name: "Basic usage",
+      description: "Assuming the following HTML form:\n\
+``<form>\n\
+  <input type=\"text\" id=\"txt\" value=\"hello\">\n\
+  <input type=\"number\" id=\"num\" value=\"5\">\n\
+</form>``\n\
+Code:\n\
+``var ctrls = findEverything();\n\
+var state = readForm(ctrls);\n\
+console.assert(state.txt === \"hello\");\n\
+console.assert(state.num === \"5\");``"
+  }]
 } );
 function readForm ( ctrls ) {
   var state = {};
@@ -126,8 +140,7 @@ function readForm ( ctrls ) {
       var c = ctrls[name];
       if ( ( c.tagName === "INPUT" || c.tagName === "SELECT" ) &&
           ( !c.dataset || !c.dataset.skipcache ) ) {
-        if ( c.type === "text" || c.type === "password" || c.tagName ===
-            "SELECT" ) {
+        if ( c.type === "text" || c.type === "password" || c.tagName === "SELECT" ) {
           state[name] = c.value;
         }
         else if ( c.type === "checkbox" || c.type === "radio" ) {
