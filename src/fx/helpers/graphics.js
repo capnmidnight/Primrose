ba5aea93944453f4bb819b994398256fe2cbd924
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-02-23 22:51:41 -0500

    An example of using the shell() function

--- src/fx/helpers/graphics.js
+++ src/fx/helpers/graphics.js
@@ -12,10 +12,10 @@ pliny.class( "", {
     {name: "radius", type: "Number", description: "How far the sphere should extend away from a center point."},
     {name: "widthSegments", type: "Number", description: "The number of faces wide in which to slice the geometry."},
     {name: "heightSegments", type: "Number", description: "The number of faces tall in which to slice the geometry."},
-    {name: "phiStart", type: "Number", description: "The angle around the Y-axis at which the sphere starts."},
-    {name: "phiLength", type: "Number", description: "The change in angle around the Y-axis to which the sphere ends."},
-    {name: "thetaStart", type: "Number", description: "The angle around the Z-axis at which the sphere starts."},
-    {name: "thetaLength", type: "Number", description: "The change in angle around the Z-axis to which the sphere ends."}
+    {name: "phiStart", type: "Number", description: "The angle in radians around the Y-axis at which the sphere starts."},
+    {name: "phiLength", type: "Number", description: "The change of angle in radians around the Y-axis to which the sphere ends."},
+    {name: "thetaStart", type: "Number", description: "The angle in radians around the Z-axis at which the sphere starts."},
+    {name: "thetaLength", type: "Number", description: "The change of angle in radians around the Z-axis to which the sphere ends."}
   ],
   description: "The InsideSphereGeometry is basically an inside-out Sphere. Or\n\
 more accurately, it's a Sphere where the face winding order is reversed, so that\n\
@@ -173,13 +173,44 @@ pliny.class( "", {
     {name: "radius", type: "Number", description: "How far the sphere should extend away from a center point."},
     {name: "widthSegments", type: "Number", description: "The number of faces wide in which to slice the geometry."},
     {name: "heightSegments", type: "Number", description: "The number of faces tall in which to slice the geometry."},
-    {name: "phi", type: "Number", description: "The angle width around the Y-axis of the sphere."},
-    {name: "thetaStart", type: "Number", description: "The angle width around the Z-axis of the sphere."}
+    {name: "phi", type: "Number", description: "The angle in radians around the Y-axis of the sphere."},
+    {name: "thetaStart", type: "Number", description: "The angle in radians around the Z-axis of the sphere."}
   ],
-  description: "The shell is basically an inside-out Sphere. Say you want a to model\n\
+  description: "The shell is basically an inside-out sphere. Say you want a to model\n\
 the sky as a sphere, or the inside of a helmet. You don't care anything about the\n\
 outside of this sphere, only the inside. You would use InsideSphereGeometry in this\n\
-case. It is mostly an alias for [`InsideSphereGeometry`](#InsideSphereGeometry)."
+case. It is mostly an alias for [`InsideSphereGeometry`](#InsideSphereGeometry).",
+  examples: [
+    {name: "Create a sky sphere", description: "To create a sphere that hovers around the user at a\n\
+far distance, showing a sky of some kind, you can use the `shell()` function in\n\
+combination with the [`textured()`](#textured) function. Assuming you have an image\n\
+file to use as the texture, execute code as such:\n\
+\n\
+    grammar(\"JavaScript\");\n\
+    var sky = textured(\n\
+      shell(\n\
+          // The radius value should be less than your draw distance.\n\
+          1000,\n\
+          // The number of slices defines how smooth the sphere will be in the\n\
+          // horizontal direction. Think of it like lines of longitude.\n\
+          18,\n\
+          // The number of rinigs defines how smooth the sphere will be in the\n\
+          // vertical direction. Think of it like lines of latitude.\n\
+          9,\n\
+          // The phi angle is the number or radians around the 'belt' of the sphere\n\
+          // to sweep out the geometry. To make a full circle, you'll need 2 * PI\n\
+          // radians.\n\
+          Math.PI * 2,\n\
+          // The theta angle is the number of radians above and below the 'belt'\n\
+          // of the sphere to sweep out the geometry. Since the belt sweeps a full\n\
+          // 360 degrees, theta only needs to sweep a half circle, or PI radians.\n\
+          Math.PI ),\n\
+      // Specify the texture image next.\n\
+      \"skyTexture.jpg\",\n\
+      // Specify that the material should be shadeless, i.e. no shadows. This\n\
+      // works best for skymaps.\n\
+      true );" }
+  ]
 } );
 function shell ( r, slices, rings, phi, theta ) {
   var SLICE = 0.45;
