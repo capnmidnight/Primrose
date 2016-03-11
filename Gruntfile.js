8f6bbb31300f4bbc8240a139f5e8d0023181f7ed
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-03-11 14:52:18 -0500

    css minifier

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,30 +2,18 @@
 
 var pathX = /.*\/(.*).js/,
     pkg = require("./package.json"),
-    files = [
+    baseFiles = [
       "obj/Primrose.js",
       "lib/analytics.js",
       "lib/ga.js",
       "lib/mailchimp.js",
       "lib/pliny.js",
-      "lib/loader.js",
-      "lib/resizer.js",
-      "node_modules/cannon/build/cannon.js",
       "node_modules/leapjs/leap-0.6.4.js",
       "node_modules/socket.io-client/socket.io.js",
       "node_modules/three/three.js",
       "node_modules/marked/lib/marked.js"
     ],
-    buildFiles = {
-      "obj/Primrose.js": ["lib/pliny.js", "src/index.js", "src/fx/**/*.js"]
-    },
-    uglifyFiles = files.map(function (s) {
-      return {
-        src: s,
-        dest: s.replace(pathX, "scripts/$1.min.js")
-      };
-    }),
-    copyFiles = files.map(function (s) {
+    copyFiles = baseFiles.map(function (s) {
       return {
         src: s,
         dest: s.replace(pathX, "scripts/$1.js")
@@ -46,15 +34,13 @@ copyFiles.push({
 function jadeConfiguration(options, defaultData, appendData) {
   var config = {
     options: options,
-    files: [
-      {
+    files: [{
         expand: true,
         src: ["**/*.jade"],
         dest: "",
         ext: "",
         extDot: "last"
-      }
-    ]
+      }]
   };
   
   config.options.data = function (dest, src) {
@@ -79,7 +65,7 @@ module.exports = function (grunt) {
     
     pkg: grunt.file.readJSON("package.json"),
     
-    clean: ["obj", "scripts", "debug", "release"],
+    clean: ["obj", "scripts", "debug", "release", "doc/**/*.min.css", "examples/**/*.min.css", "stylesheets/**/*.min.css"],
     
     jade: {
       release: jadeReleaseConfiguration,
@@ -107,13 +93,31 @@ module.exports = function (grunt) {
             "console.log(\"Using Primrose v<%= pkg.version %>. Find out more at <%= pkg.homepage %>\");"
       },
       default: {
-        files: buildFiles
+        files: {
+          "obj/Primrose.js": ["lib/pliny.js", "src/index.js", "src/fx/**/*.js"]
+        }
+      }
+    },
+    
+    cssmin: {
+      default: {
+        files: [{
+            expand: true,
+            src: ["doc/**/*.css", "stylesheets/**/*.css", "examples/**/*.css", "!*.min.css"],
+            dest: "",
+            ext: ".min.css"
+          }]
       }
     },
     
     uglify: {
       default: {
-        files: uglifyFiles
+        files: baseFiles.map(function (s) {
+          return {
+            src: s,
+            dest: s.replace(pathX, "scripts/$1.min.js")
+          };
+        })
       }
     },
     
@@ -130,9 +134,10 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("grunt-contrib-concat");
   grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
   grunt.loadNpmTasks("grunt-contrib-jade");
   
   grunt.registerTask("debug", ["jade:debug"]);
-  grunt.registerTask("release", ["clean", "jade:release", "jshint", "concat", "uglify", "copy"]);
+  grunt.registerTask("release", ["clean", "jade:release", "jshint", "concat", "cssmin", "uglify", "copy"]);
   grunt.registerTask("default", ["debug"]);
 };
