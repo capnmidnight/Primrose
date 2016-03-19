81e7194a34a72263e4873eaf940d294e35d52750
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-03-19 08:57:27 -0400

    cache materials

--- src/base/textured.js
+++ src/base/textured.js
@@ -8,97 +8,106 @@ pliny.function("", {
   name: "textured",
   description: "<under construction>"
 });
-function textured(geometry, txt, unshaded, o, s, t) {
-  var material, surface;
-  if (o === undefined) {
-    o = 1;
-  }
-
-  if (typeof txt === "number") {
-    if (unshaded) {
-      material = new THREE.MeshBasicMaterial({
-        transparent: true,
-        color: txt,
-        opacity: o,
-        shading: THREE.FlatShading
-      });
-    }
-    else {
-      material = new THREE.MeshLambertMaterial({
-        transparent: true,
-        color: txt,
-        opacity: o
-      });
-    }
-  }
-  else if (txt instanceof Primrose.Surface) {
-    material = txt.material;
-    surface = txt;
-  }
-  else {
-    if (unshaded) {
-      material = new THREE.MeshBasicMaterial({
-        color: 0xffffff,
-        transparent: true,
-        shading: THREE.FlatShading,
-        side: THREE.DoubleSide,
-        opacity: o
-      });
-    }
-    else {
-      material = new THREE.MeshLambertMaterial({
-        color: 0xffffff,
-        transparent: true,
-        side: THREE.DoubleSide,
-        opacity: o
-      });
+var textured = (function () {
+  var cache = {};
+  function textured(geometry, txt, unshaded, o, s, t) {
+    var material, surface;
+    if (o === undefined) {
+      o = 1;
     }
 
-    var setTexture = function (texture) {
-      if (!texture) {
-        console.log(txt);
-        console.trace();
+    if (cache[txt]) {
+      console.log("using cached material");
+      material = cache[txt];
+    }
+    else if (typeof txt === "number") {
+      if (unshaded) {
+        material = new THREE.MeshBasicMaterial({
+          transparent: true,
+          color: txt,
+          opacity: o,
+          shading: THREE.FlatShading
+        });
       }
       else {
-        material.map = texture;
-        material.needsUpdate = true;
-        if (s * t > 1) {
-          texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-          texture.repeat.set(s, t);
-        }
+        material = new THREE.MeshLambertMaterial({
+          transparent: true,
+          color: txt,
+          opacity: o
+        });
       }
-    };
-    
-    if (typeof txt === "string") {
-      Primrose.loadTexture(txt, setTexture,
-        null,
-        console.error.bind(console, "Error loading texture", txt));
     }
-    else if (txt instanceof Primrose.Text.Controls.TextBox) {
-      setTexture(txt.renderer.texture);
-    }
-    else if (txt instanceof HTMLCanvasElement) {
-      var txt2 = new THREE.Texture(txt);
-      txt2.needsUpdate = true;
-      setTexture(txt2);
+    else if (txt instanceof Primrose.Surface) {
+      material = txt.material;
+      surface = txt;
     }
     else {
-      setTexture(txt);
+      if (unshaded) {
+        material = new THREE.MeshBasicMaterial({
+          color: 0xffffff,
+          transparent: true,
+          shading: THREE.FlatShading,
+          side: THREE.DoubleSide,
+          opacity: o
+        });
+      }
+      else {
+        material = new THREE.MeshLambertMaterial({
+          color: 0xffffff,
+          transparent: true,
+          side: THREE.DoubleSide,
+          opacity: o
+        });
+      }
+
+      var setTexture = function (texture) {
+        if (!texture) {
+          console.log(txt);
+          console.trace();
+        }
+        else {
+          material.map = texture;
+          material.needsUpdate = true;
+          if (s * t > 1) {
+            texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+            texture.repeat.set(s, t);
+          }
+        }
+      };
+
+      if (typeof txt === "string") {
+        Primrose.loadTexture(txt, setTexture,
+          null,
+          console.error.bind(console, "Error loading texture", txt));
+      }
+      else if (txt instanceof Primrose.Text.Controls.TextBox) {
+        setTexture(txt.renderer.texture);
+      }
+      else if (txt instanceof HTMLCanvasElement) {
+        var txt2 = new THREE.Texture(txt);
+        txt2.needsUpdate = true;
+        setTexture(txt2);
+      }
+      else {
+        setTexture(txt);
+      }
     }
+
+    var obj = null;
+    if (geometry.type.indexOf("Geometry") > -1) {
+      obj = new THREE.Mesh(geometry, material);
+    }
+    else if (geometry instanceof THREE.Object3D) {
+      geometry.material = material;
+      obj = geometry;
+    }
+
+    if (surface) {
+      obj.surface = surface;
+    }
+    cache[txt] = material;
+    return obj;
   }
 
-  var obj = null;
-  if (geometry.type.indexOf("Geometry") > -1) {
-    obj = new THREE.Mesh(geometry, material);
-  }
-  else if (geometry instanceof THREE.Object3D) {
-    geometry.material = material;
-    obj = geometry;
-  }
-
-  if (surface) {
-    obj.surface = surface;
-  }
-
-  return obj;
-}
+  return textured;
+})();
