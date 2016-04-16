3ad6ea5192ab8dd1ae52cc0a5e24d479cfa17b7b
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-04-16 15:16:57 -0400

    make logger easier to determine issue source

--- server/controllers/logger.js
+++ server/controllers/logger.js
@@ -3,6 +3,7 @@ module.exports = {
   POST: function (params, sendData, serverError, body) {
     var func = console[body.name];
     if (func) {
+      body.args.unshift(body.name.toLocaleUpperCase() + ":> ");
       func.apply(console, body.args);
     }
     else {
