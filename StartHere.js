e61b1624da2211a76f5136380d0c00cee5fb9231
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-05-13 14:26:49 -0400

    make startup script not open the browser, because Visual Studio is going to do that for us.

--- StartHere.js
+++ StartHere.js
@@ -1,1 +1,6 @@
-﻿require("child_process").execFile("quickstart/StartHere.exe", process.argv);
+﻿var args = process.argv.splice(2);
+if (process.env.PORT) {
+  args.push("--port");
+  args.push(process.env.PORT);
+}
+require("child_process").execFile("quickstart/StartHere.exe", args);
