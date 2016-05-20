96bf2f1789f1ddddcd5cee4ac7efd9b9adfd45a6
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-05-19 20:58:36 -0400

    rename "app" to "env", as it makes more sense

--- quickstart/app.js
+++ quickstart/app.js
@@ -1,6 +1,6 @@
 THREE.ImageLoader.crossOrigin = "anonymous";
 
-var app = new Primrose.BrowserEnvironment( "My Primrose VR Application", {
+var env = new Primrose.BrowserEnvironment( "My Primrose VR Application", {
     skyTexture: "bg.jpg",
     groundTexture: "deck.png",
     fullScreenIcon: "monitor.obj",
@@ -8,24 +8,24 @@ var app = new Primrose.BrowserEnvironment( "My Primrose VR Application", {
     font: "helvetiker_regular.typeface.js"
   } );
 
-app.addEventListener( "ready", function () {
+env.addEventListener( "ready", function () {
   // Perform any post-initialization setup. Once this event fires, the Primrose
   // framework is ready and will start animation as soon as this function returns.
 } );
 
-app.addEventListener( "gazecomplete", function(evt){
+env.addEventListener( "gazecomplete", function(evt){
   // You can respond to "intended stare" events here, i.e. when the user gazes
   // at a particular object for an extended period of time. Usually, about three
   // seconds.
 } );
 
-app.addEventListener( "pointerend", function(evt){
+env.addEventListener( "pointerend", function(evt){
   // You can respond to the user "clicking" an object here. This could be by using
   // a mouse on their desktop PC or by touching the screen while looking at an
   // object on a mobile device.
 } );
 
-app.addEventListener( "update", function ( dt ) {
+env.addEventListener( "update", function ( dt ) {
   // Perform per-frame updates here, like moving objects around according to your
   // own rules.
 } );
