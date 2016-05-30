04ca64759f0f7e8b628c86840d20cce627201d24
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-05-30 16:56:55 -0400

    meeting room

--- meeting/app.js
+++ meeting/app.js
@@ -1,35 +1,44 @@
 var idSpec = location.search.match(/id=(\w+)/),
-meetingID = idSpec && idSpec[1] || "public",
-env = new Primrose.BrowserEnvironment( "Meeting:" + meetingID, {
-    skyTexture: "../doc/images/bg.jpg",
-    groundTexture: "../doc/images/deck.png",
+  meetingID = idSpec && idSpec[1] || "public",
+  env = new Primrose.BrowserEnvironment("Meeting:" + meetingID, {
+    skyTexture: 0xffffcc,
+    backgroundColor: 0xffffcc,
+    disableDefaultLighting: true,
+    sceneModel: "../doc/models/meeting/meetingroom.obj",
+    useFog: true,
     fullScreenIcon: "../doc/models/monitor.obj",
     VRIcon: "../doc/models/cardboard.obj",
     font: "../doc/fonts/helvetiker_regular.typeface.js"
   }),
   login = new Primrose.X.LoginForm();
 
-login.addEventListener( "login", function(socket){
+login.addEventListener("login", function (socket) {
   env.socket = socket;
 });
 
-login.addEventListener( "logout", function(){
+login.addEventListener("logout", function () {
   env.socket = null;
 });
 
-env.addEventListener( "ready", function (){
+env.addEventListener("ready", function () {
   login.mesh.position.set(-1, env.avatarHeight, -1.5);
   env.appendChild(login);
+
+  env.scene.traverse(function (obj) {
+    if (obj.name.indexOf("Chair") === 0) {
+      env.registerPickableObject(obj);
+    }
+  });
 });
 
-env.addEventListener( "gazecomplete", function(evt){
+env.addEventListener("gazecomplete", function (evt) {
 
 });
 
-env.addEventListener( "pointerend", function(evt){
+env.addEventListener("pointerend", function (evt) {
 
 });
 
-env.addEventListener( "update", function (dt){
+env.addEventListener("update", function (dt) {
 
 });
