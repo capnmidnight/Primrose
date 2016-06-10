5f5d14b211e6b27b140ee90101eb2128ae8b64e5
Sean McBeth <Sean McBeth>
2016-06-10 15:59:24 -0400

    new IconManager class

---
+++ src/fx/text/IconManager.js
@@ -0,0 +1,11 @@
+﻿"use strict";
+
+pliny.class({
+  parent: "Primrose",
+  name: "IconManager",
+  description: "Handles the loading and instantiation of icons in 3D space.",
+  parameters: [
+    { name: "options", type: "Object", description: "Options for the model files and type constructors for each of the icon types." },
+    { name: "options", type: "Object", description: "Options for the model files and type constructors for each of the icon types." },
+  ]
+});
