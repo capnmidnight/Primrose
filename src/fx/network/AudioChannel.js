84f69d45c89f2766ee1573db2555b52a4f55c29a
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-08-05 14:05:30 -0400

    make the "preferOpus" path a configurable option

--- src/fx/network/AudioChannel.js
+++ src/fx/network/AudioChannel.js
@@ -1,6 +1,8 @@
 Primrose.Network.AudioChannel = (function () {
   "use strict";
 
+  const ENABLE_OPUS_HACK = true;
+
   if (!navigator.mediaDevices) {
     navigator.mediaDevices = {};
   }
@@ -14,71 +16,77 @@ Primrose.Network.AudioChannel = (function () {
     navigator.mediaDevices.getUserMedia = (constraint) => new Promise((resolve, reject) => navigator.getUserMedia(constraint, resolve, reject));
   }
 
-  function preferOpus(description) {
-    var sdp = description.sdp;
-    var sdpLines = sdp.split('\r\n');
-    var mLineIndex = null;
-    // Search for m line.
-    for (var i = 0; i < sdpLines.length; i++) {
-      if (sdpLines[i].search('m=audio') !== -1) {
-        mLineIndex = i;
-        break;
-      }
-    }
-    if (mLineIndex === null) return sdp;
-
-    // If Opus is available, set it as the default in m line.
-    for (var j = 0; j < sdpLines.length; j++) {
-      if (sdpLines[j].search('opus/48000') !== -1) {
-        var opusPayload = extractSdp(sdpLines[j], /:(\d+) opus\/48000/i);
-        if (opusPayload) sdpLines[mLineIndex] = setDefaultCodec(sdpLines[mLineIndex], opusPayload);
-        break;
-      }
-    }
-
-    // Remove CN in m line and sdp.
-    sdpLines = removeCN(sdpLines, mLineIndex);
-
-    description.sdp = sdpLines.join('\r\n');
-    return description;
-  }
-
-  function extractSdp(sdpLine, pattern) {
-    var result = sdpLine.match(pattern);
-    return (result && result.length == 2) ? result[1] : null;
-  }
-
-  function setDefaultCodec(mLine, payload) {
-    var elements = mLine.split(' ');
-    var newLine = [];
-    var index = 0;
-    for (var i = 0; i < elements.length; i++) {
-      if (index === 3) // Format of media starts from the fourth.
-        newLine[index++] = payload; // Put target payload to the first.
-      if (elements[i] !== payload) newLine[index++] = elements[i];
-    }
-    return newLine.join(' ');
-  }
-
-  function removeCN(sdpLines, mLineIndex) {
-    var mLineElements = sdpLines[mLineIndex].split(' ');
-    // Scan from end for the convenience of removing an item.
-    for (var i = sdpLines.length - 1; i >= 0; i--) {
-      var payload = extractSdp(sdpLines[i], /a=rtpmap:(\d+) CN\/\d+/i);
-      if (payload) {
-        var cnPos = mLineElements.indexOf(payload);
-        if (cnPos !== -1) {
-          // Remove CN payload from m line.
-          mLineElements.splice(cnPos, 1);
+  var preferOpus = (function(){
+    function preferOpus(description) {
+      if(ENABLE_OPUS_HACK){
+        var sdp = description.sdp;
+        var sdpLines = sdp.split('\r\n');
+        var mLineIndex = null;
+        // Search for m line.
+        for (var i = 0; i < sdpLines.length; i++) {
+          if (sdpLines[i].search('m=audio') !== -1) {
+            mLineIndex = i;
+            break;
+          }
         }
-        // Remove CN line in sdp
-        sdpLines.splice(i, 1);
+        if (mLineIndex === null) return sdp;
+
+        // If Opus is available, set it as the default in m line.
+        for (var j = 0; j < sdpLines.length; j++) {
+          if (sdpLines[j].search('opus/48000') !== -1) {
+            var opusPayload = extractSdp(sdpLines[j], /:(\d+) opus\/48000/i);
+            if (opusPayload) sdpLines[mLineIndex] = setDefaultCodec(sdpLines[mLineIndex], opusPayload);
+            break;
+          }
+        }
+
+        // Remove CN in m line and sdp.
+        sdpLines = removeCN(sdpLines, mLineIndex);
+
+        description.sdp = sdpLines.join('\r\n');
       }
+      return description;
     }
 
-    sdpLines[mLineIndex] = mLineElements.join(' ');
-    return sdpLines;
-  }
+    function extractSdp(sdpLine, pattern) {
+      var result = sdpLine.match(pattern);
+      return (result && result.length == 2) ? result[1] : null;
+    }
+
+    function setDefaultCodec(mLine, payload) {
+      var elements = mLine.split(' ');
+      var newLine = [];
+      var index = 0;
+      for (var i = 0; i < elements.length; i++) {
+        if (index === 3) // Format of media starts from the fourth.
+          newLine[index++] = payload; // Put target payload to the first.
+        if (elements[i] !== payload) newLine[index++] = elements[i];
+      }
+      return newLine.join(' ');
+    }
+
+    function removeCN(sdpLines, mLineIndex) {
+      var mLineElements = sdpLines[mLineIndex].split(' ');
+      // Scan from end for the convenience of removing an item.
+      for (var i = sdpLines.length - 1; i >= 0; i--) {
+        var payload = extractSdp(sdpLines[i], /a=rtpmap:(\d+) CN\/\d+/i);
+        if (payload) {
+          var cnPos = mLineElements.indexOf(payload);
+          if (cnPos !== -1) {
+            // Remove CN payload from m line.
+            mLineElements.splice(cnPos, 1);
+          }
+          // Remove CN line in sdp
+          sdpLines.splice(i, 1);
+        }
+      }
+
+      sdpLines[mLineIndex] = mLineElements.join(' ');
+      return sdpLines;
+    }
+
+    return preferOpus;
+  })();
 
   let INSTANCE_COUNT = 0;
 
