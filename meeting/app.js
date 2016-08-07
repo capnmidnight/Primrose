669e4980658e25d7ddf5f32f9ddefba98b2b0765
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-08-07 07:53:55 -0400

    simple meeting room login

--- meeting/app.js
+++ meeting/app.js
@@ -1,26 +1,17 @@
 "use strict";
 
-var ERROR_MESSAGES = {
-    login: "We couldn't log you in right now because ",
-    signup: "We couldn't sign you up right now because "
-  },
+var ctrls = Primrose.DOM.findEverything(),
+  loginControls = [
+    ctrls.userName,
+    ctrls.connect
+  ],
   names = NameGen.compile("!mi"),
-  MEETING_ID_PATTERN = /\bid=(\w+)/,
-  idSpec = location.search.match(MEETING_ID_PATTERN),
-  hasMeetingID = !!idSpec,
-  meetingID = idSpec && idSpec[1] || names.toString(),
-  USER_NAME_PATTERN = /Primrose:user:(\w+)/,
-  userNameSpec = document.cookie.match(USER_NAME_PATTERN),
-  userName = userNameSpec && userNameSpec[1] || names.toString(),
   protocol = location.protocol.replace("http", "ws"),
   serverPath = protocol + "//" + location.hostname,
   socket = null,
-  ctrls2D = Primrose.DOM.findEverything(),
-  loginControls = ["email", "password", "userName", "switchMode", "connect"].map(function (name) {
-    return ctrls2D[name];
-  }),
+  meetingID = null,
 
-  env = new Primrose.BrowserEnvironment(getAppKey(), {
+  env = new Primrose.BrowserEnvironment(meetingID, {
     autoScaleQuality: true,
     autoRescaleQuality: false,
     quality: Primrose.Quality.HIGH,
@@ -36,64 +27,37 @@ var ERROR_MESSAGES = {
     font: "../doc/fonts/helvetiker_regular.typeface.js"
   });
 
-function getAppKey() {
-  return "Primrose:Meeting:" + meetingID.toLocaleUpperCase();
-}
-
-function makeURL() {
-  if (!hasMeetingID) {
-    var state = "?id=" + meetingID;
-    if (hasTestUser) {
-      state += "&u=" + userName;
-      if(hasTestPassword) {
-        state += "&p=" + testPassword;
-      }
-    }
-    history.pushState(null, "Room ID: " + meetingID, state);
-  }
-}
-
-makeURL();
-
-ctrls2D.room.value = meetingID;
-ctrls2D.userName.value = userName;
-
-showSignup(userName.length === 0);
-
-ctrls2D.switchMode.addEventListener("click", showSignup);
-ctrls2D.connect.addEventListener("click", authenticate);
-ctrls2D.room.addEventListener("change", authenticate);
-ctrls2D.userName.addEventListener("keyup", authenticate);
-ctrls2D.password.addEventListener("keyup", authenticate);
-ctrls2D.closeButton.addEventListener("click", hideLoginForm, false);
+ctrls.closeButton.addEventListener("click", hideLoginForm, false);
+ctrls.userName.addEventListener("keyup", authenticate);
+ctrls.connect.addEventListener("click", authenticate);
 
 env.addEventListener("ready", environmentReady);
 
-function showSignup(state) {
-  if (typeof state !== "boolean") {
-    state = ctrls2D.emailRow.style.display === "none";
-  }
 
-  ctrls2D.controls.style.width = "100%";
-  ctrls2D.controls.style.height = "100%";
-  ctrls2D.emailRow.style.display = state ? "" : "none";
-  ctrls2D.switchMode.innerHTML = state ? "Log in" : "Sign up";
-  ctrls2D.switchMode.className = state ? "loginButton" : "signupButton";
-  ctrls2D.connect.innerHTML = state ? "Sign up" : "Log in";
-  ctrls2D.connect.className = state ? "signupButton" : "loginButton";
+ctrls.userName.value = fromField(document.cookie, /user=(\w+)/);
+
+function fromField(field, pattern) {
+  var spec = field.match(pattern);
+  return spec && spec[1];
 }
 
 function hideLoginForm() {
-  ctrls2D.loginForm.style.display = "none";
-  ctrls2D.frontBuffer.focus();
+  ctrls.loginForm.style.display = "none";
+  ctrls.frontBuffer.focus();
+}
+
+function showLoginForm() {
+  ctrls.loginForm.style.display = "";
+  ctrls.userName.focus();
 }
 
 function errorMessage(message) {
-  if (!ctrls2D.loginForm.style.width) {
-    ctrls2D.loginForm.style.width = ctrls2D.loginForm.clientWidth + "px";
+  if (!ctrls.loginForm.style.width) {
+    ctrls.loginForm.style.width = ctrls.loginForm.clientWidth + "px";
   }
-  ctrls2D.errorMessage.innerHTML = message;
-  ctrls2D.errorMessage.style.display = "block";
+  ctrls.errorMessage.innerHTML = message;
+  ctrls.errorMessage.style.display = "block";
+  showLoginForm();
   disableLogin(false);
 }
 
@@ -105,7 +69,7 @@ function disableLogin(v) {
 }
 
 function environmentReady() {
-  ctrls2D.loginForm.style.display = "";
+  ctrls.loginForm.style.display = "";
 
   env.scene.traverse(function (obj) {
     if (obj.name.indexOf("LightPanel") === 0) {
@@ -116,88 +80,53 @@ function environmentReady() {
 
 function authenticate(evt) {
   if (evt.type !== "keyup" || evt.keyCode === 13) {
-    var verb = ctrls2D.emailRow.style.display === "none" ? "login" : "signup",
-      password = ctrls2D.password.value,
-      email = ctrls2D.email.value;
 
-    meetingID = ctrls2D.room.value;
-    userName = ctrls2D.userName.value.toLocaleUpperCase();
-    makeURL();
     disableLogin(true);
 
-    if (userName.length === 0) {
-      errorMessage("You must provide a user name.");
+    meetingID = fromField(location.search, /\broom=(\w+)/);
+    if(!meetingID){
+      meetingID = "Primrose:Meeting:" + Primrose.Random.ID();
+      var state = "?room=" + meetingID;
+      history.pushState(null, "Room ID: " + meetingID, state);
     }
-    else if (password.length === 0) {
-      errorMessage("You must provide a password.");
-    }
-    else {
-      if (!socket) {
-        console.log("connecting to: %s", serverPath);
-        socket = io(serverPath);
-        socket.on("connect_error", connectionError.bind(null, verb));
-        socket.on("signupFailed", authFailed("signup"));
-        socket.on("loginFailed", authFailed("login"));
-        socket.on("logoutComplete", showSignup.bind(null, false));
-        socket.on("errorDetail", console.error.bind(console));
-        socket.on("loginComplete", authSucceeded);
-      }
 
-      socket.once("salt", function(salt) {
-        var hash = new Hashes.SHA256()
-          .hex(salt + password)
-        socket.emit("hash", hash);
-      });
-      socket.emit(verb, {
-        userName: userName,
-        email: email,
-        app: getAppKey()
-      });
+    if(ctrls.userName.value.length === 0){
+      ctrls.userName.value = names.toString();
     }
+
+    if (!socket) {
+      console.log("connecting to: %s", serverPath);
+      socket = io(serverPath);
+      socket.on("connect_error", connectionError);
+      socket.on("loginFailed", authFailed);
+      socket.on("loginComplete", authSucceeded);
+      socket.on("errorDetail", console.error.bind(console));
+    }
+
+    socket.emit("guest", {
+      userName: ctrls.userName.value,
+      appKey: meetingID
+    });
   }
 }
 
-function connectionError(verb, evt) {
+function connectionError(evt) {
   socket.close();
   socket = null;
   env.disconnect();
-  authFailed(verb)("an error occured while connecting to the server.");
+  authFailed("an error occured while connecting to the server.");
 }
 
-function authFailed(verb) {
-  return function(reason) {
-    showSignup(verb === "signup");
-    errorMessage(ERROR_MESSAGES[verb] + reason.replace(/\[USER\]/g, ctrls2D.userName.value));
-  }
+function authFailed(reason) {
+  errorMessage("We couldn't log you in right now because " + reason.replace(/\[USER\]/g, ctrls.userName.value));
 }
 
 function authSucceeded() {
-  ctrls2D.errorMessage.innerHTML = "";
-  ctrls2D.errorMessage.style.display = "none";
+  ctrls.errorMessage.innerHTML = "";
+  ctrls.errorMessage.style.display = "none";
   disableLogin(false);
   hideLoginForm();
 
-  document.cookie = "Primrose:user:" + userName;
-  env.connect(socket, userName);
+  document.cookie = "user=" + ctrls.userName.value;
+  env.connect(socket, ctrls.userName.value);
 }
-
-
-
-////////////////////////////////////////////////////////////////////////
-///////     setting up test user accounts    ///////////////////////////
-////////////////////////////////////////////////////////////////////////
-var TEST_USER_NAME_PATTERN = /\bu=(\w+)/,
-  testUserNameSpec = location.search.match(TEST_USER_NAME_PATTERN),
-  hasTestUser = !!testUserNameSpec,
-  userName = testUserNameSpec && testUserNameSpec[1] || userName,
-  TEST_PASSWORD_PATTERN = /\bp=(\w+)/,
-  testPasswordSpec = location.search.match(TEST_PASSWORD_PATTERN),
-  hasTestPassword = !!testPasswordSpec,
-  testPassword = testPasswordSpec && testPasswordSpec[1] || null;
-ctrls2D.userName.value = userName;
-ctrls2D.password.value = testPassword;
-
-showSignup(userName.length === 0);
-////////////////////////////////////////////////////////////////////////
-///////     end setting up test user accounts    ///////////////////////
-////////////////////////////////////////////////////////////////////////
