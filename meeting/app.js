df4764147e88e4a8b08b9982217f322e4c343535
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-08-15 14:10:52 -0400

    room name selection

--- meeting/app.js
+++ meeting/app.js
@@ -8,8 +8,11 @@ var ctrls = Primrose.DOM.findEverything(),
   names = NameGen.compile("!mi"),
   protocol = location.protocol.replace("http", "ws"),
   serverPath = protocol + "//" + location.hostname,
+  roomPattern = /\broom=(\w+)/,
+  userPattern = /\buser=(\w+)/,
+  defaultRoomName = null,
+  defaultUserName = null,
   socket = null,
-  meetingID = null,
 
   env = new Primrose.BrowserEnvironment({
     autoScaleQuality: true,
@@ -31,11 +34,41 @@ var ctrls = Primrose.DOM.findEverything(),
 ctrls.closeButton.addEventListener("click", hideLoginForm, false);
 ctrls.userName.addEventListener("keyup", authenticate);
 ctrls.connect.addEventListener("click", authenticate);
+ctrls.randomRoomName.addEventListener("click", setRoomName);
+ctrls.randomUserName.addEventListener("click", setRandomUserName);
+
+window.addEventListener("popstate", setRoomName);
 
 env.addEventListener("ready", environmentReady);
 
+setRoomName();
+setRandomUserName();
 
-ctrls.userName.value = fromField(location.search, /\buser=(\w+)/) || fromField(document.cookie, /\buser=(\w+)/);
+ctrls.roomName.value = fromField(location.search, roomPattern) || fromField(document.cookie, roomPattern);
+ctrls.userName.value = fromField(location.search, userPattern) || fromField(document.cookie, userPattern);
+
+function setRoomName(evt) {
+  defaultRoomName = evt && evt.state && evt.state.roomName || names.toString();
+  ctrls.roomName.placeholder = "Type a room name (currently " + defaultRoomName + ")";
+  ctrls.roomName.value = "";
+  if(evt && evt.type !== "popstate"){
+    history.pushState({ roomName: defaultRoomName }, "Room ID: " + defaultRoomName, "?room=" + defaultRoomName);
+  }
+}
+
+function setRandomUserName() {
+  defaultUserName = names.toString();
+  ctrls.userName.placeholder = "Type a user name (currently " + defaultUserName + ")";
+  ctrls.userName.value = "";
+}
+
+function getRoomName() {
+  return ctrls.roomName.value || defaultRoomName;
+}
+
+function getUserName() {
+  return ctrls.userName.value || defaultUserName;
+}
 
 function fromField(field, pattern) {
   var spec = field.match(pattern);
@@ -81,17 +114,6 @@ function authenticate(evt) {
 
     disableLogin(true);
 
-    meetingID = fromField(location.search, /\broom=(\w+)/);
-    if(!meetingID){
-      meetingID =  Primrose.Random.ID();
-      var state = "?room=" + meetingID;
-      history.pushState(null, "Room ID: " + meetingID, state);
-    }
-
-    if(ctrls.userName.value.length === 0){
-      ctrls.userName.value = names.toString();
-    }
-
     if (!socket) {
       console.log("connecting to: %s", serverPath);
       socket = io(serverPath);
@@ -103,8 +125,8 @@ function authenticate(evt) {
     }
 
     socket.emit("guest", {
-      userName: ctrls.userName.value,
-      appKey: meetingID
+      userName: getUserName(),
+      appKey: getRoomName()
     });
   }
 }
@@ -126,6 +148,7 @@ function authSucceeded() {
   disableLogin(false);
   hideLoginForm();
 
-  document.cookie = "user=" + ctrls.userName.value;
+  document.cookie = "user=" + getUserName() +
+    "\nroom=" + getRoomName();
   env.connect(socket, ctrls.userName.value);
 }
