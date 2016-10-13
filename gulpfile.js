b2ec843b3e3d0681dbd9e6cd5fb9d4b7a6ad8485
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-10-13 14:15:31 -0400

    not using manifest feature anymore

--- gulpfile.js
+++ gulpfile.js
@@ -33,25 +33,6 @@ var gulp = require("gulp"),
   css = nt.css("Primrose", ["*.styl", "doc/**/*.styl"]);
 
 gulp.task("format", [js.format]);
-gulp.task("MeetingManifest", [tot.build], build.exec("cd meeting && node ../node_modules/webvr-bootstrapper \
-../doc/models/meeting/meetingroom.obj \
-../doc/models/meeting/meetingroom.mtl \
-../doc/models/meeting/BackdropTexture.png \
-../doc/models/meeting/Chair1Texture.png \
-../doc/models/meeting/Chair2Texture.png \
-../doc/models/meeting/Chair3Texture.png \
-../doc/models/meeting/Chair4Texture.png \
-../doc/models/meeting/Cup1Texture.png \
-../doc/models/meeting/Cup2Texture.png \
-../doc/models/meeting/Cup3Texture.png \
-../doc/models/meeting/Cup4Texture.png \
-../doc/models/meeting/Cup5Texture.png \
-../doc/models/meeting/LampshadeTexture.png \
-../doc/models/meeting/RoomTexture.png \
-../doc/models/meeting/TableTexture.png \
-../doc/fonts/helvetiker_regular.typeface.json \
-../doc/models/avatar.json \
-../doc/audio/wind.ogg app.js"));
 
 gulp.task("js", [js.default]);
 gulp.task("html", [html.debug]);
@@ -71,7 +52,7 @@ gulp.task("debug", [
 ]);
 
 gulp.task("release",  [
-  "MeetingManifest",
+  tot.build,
   html.release,
   css.build
 ]);
