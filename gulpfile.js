d033d90f0a4517ed6aaf6fde377ed408a2e65040
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-10-30 18:41:51 -0400

    build process updates

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,37 @@ var gulp = require("gulp"),
   exec = require("child_process").exec,
   pliny = require("pliny"),
   pkg = require("./package.json"),
-  build = require("notiontheory-basic-build"),
+  build = require("../notiontheory-basic-build"),
   nt = build.setup(gulp, pkg),
+
+  // The separate dependencies file exists to make the development build faster.
+  // No need to constantly be re-concatenating files that haven't changed.
   deps = nt.cat("PrimroseDependencies", [
     "node_modules/promise-polyfill/promise.js",
     "node_modules/socket.io-client/socket.io.js",
-    "../webvr-polyfill/build/webvr-polyfill.js",
-    "../webvr-standard-monitor/webvr-standard-monitor.js",
     "node_modules/three/build/three.js",
     "node_modules/three/examples/js/loaders/OBJLoader.js",
     "node_modules/iphone-inline-video/dist/iphone-inline-video.browser.js",
-    "lib/namegen.js"
+    "lib/namegen.js",
+    "../webvr-polyfill/build/webvr-polyfill.js",
+    "../webvr-standard-monitor/webvr-standard-monitor.js"
   ]),
-  js = nt.js("PrimroseLib-WithDoc", "src", ["format"], pliny.carve.bind(pliny, "PrimroseLib-WithDoc.js", "PrimroseLib.js", "PrimroseDocumentation.js"), true),
-  min = nt.min("PrimroseDocumentation", ["PrimroseDocumentation.js"], [js]),
+  js = nt.js("PrimroseLib-WithDoc", "src", ["format"], pliny.carve.bind(pliny, "PrimroseLib-WithDoc.js", "PrimroseLib.js", "doc/PrimroseDocumentation.js"), true),
+  min = nt.min("PrimroseDocumentation", ["doc/PrimroseDocumentation.js"], [js]),
   tot = nt.cat("Primrose", ["PrimroseDependencies.js", "PrimroseLib.js"], [deps, min]),
   html = nt.html("Primrose", ["*.pug", "doc/**/*.pug", "templates/**/*.pug"]),
-  css = nt.css("Primrose", ["*.styl", "doc/**/*.styl"]);
+  css = nt.css("Primrose", ["*.styl", "doc/**/*.styl"]),
+  clean = nt.clean("Primrose", [
+    "Primrose.js",
+    "PrimroseLib-WithDoc*.js",
+    "PrimroseDependencies*.js",
+    "PrimroseLib*.js"
+  ], [tot.release]);
 
 gulp.task("format", [js.format]);
 gulp.task("js", [js.default]);
+gulp.task("js:debug", [js.debug]);
+gulp.task("js:release", [js.release]);
 gulp.task("html", [html.debug]);
 gulp.task("css", [css.debug]);
 
@@ -45,7 +56,7 @@ gulp.task("test", [
 ]);
 
 gulp.task("release",  [
-  tot.release,
+  clean,
   html.release,
   css.release
 ]);
