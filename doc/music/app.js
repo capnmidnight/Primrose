59d1416c8a3b6dd2df9ce3cae778da0fab2b8413
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-11-04 17:21:01 -0400

    new music demo

--- doc/music/app.js
+++ doc/music/app.js
@@ -1,62 +1,120 @@
-var env = new Primrose.BrowserEnvironment({
-  // we want gaze-based interaction turned on
-  useGaze: true,
-  // This file references a Three.js JSON-formatted scene
-  // file, which basically describes a room or level in
-  // which the user will walk around, on which we can
-  // create additional objects.
-  sceneModel: "../models/holodeck.json",
-  font: "../fonts/helvetiker_regular.typeface.json",
-  // One of our GUI controls is configured here.
-  button: {
-    // This file references another Three.js JSON-formatted
-    // model file, which will be cloned anytime a user
-    // calls createElement("button").
-    model: "../models/smallbutton.json",
-    // Display settings for different states of the button.
-    options: {
-      colorUnpressed: 0x7f0000,
-      colorPressed: 0x007f00
-    }
-  }
-});
+var numButtons = 16,
+    height = Math.floor(Math.sqrt(numButtons)),
+    width = Math.ceil(numButtons/height),
+    midWidth = (width - 1) / 2,
+    midHeight = (height - 1) / 2,
+    colorOn = 0xffffff,
+    colorOff = 0xff0000,
+    colorPlay = 0xffff00,
+    boards = [],
+    t = 0,
+    measure = 0,
+    lastMeasure = null,
+    padSize = 0.22,
+    padDepth = 0.02,
+    textSize = 0.05,
+    perMeasure = 1 / width,
+    DIST = new THREE.Vector3(),
+    env = new Primrose.BrowserEnvironment({
+      groundTexture: "../images/deck.png",
+      font: "../fonts/helvetiker_regular.typeface.json",
+      backgroundColor: 0x000000,
+      useFog: true,
+      useGaze: isMobile,
+      drawDistance: 10
+    });
 
-
-// Primrose already has a basic sound API setup, so you
-// can hook directly into it to create basic, procedurally
-// generated music.
-function playNote(i) {
-  env.music.play(
-    // The play method understands Piano key indices, so
-    // we just need to calculate the note we want to play,
-    // understanding Middle C to be note 44.
-    35 + i * 4,
-    // Volume, on the range [0, 1]. Full volume is quite loud.
-    0.30,
-    // Duration to play, in seconds.
-    0.2);
+function text(size, text) {
+  size = size || 1;
+  return text3D(size * textSize, text).center();
 }
 
-// Once Primrose has setup the WebGL context, setup Three.js,
-// downloaded and validated all of model files, and constructed
-// the basic scene hierarchy out of it, the "ready" event is fired,
-// indicating that we may make additional changes to the scene now.
-env.addEventListener("ready", function () {
-  var numButtons = 8,
-    middle = (numButtons - 1) / 2;
-
+function Board(type){
+  this.object = hub();
+  this.title = put(text3D(0.15, type)
+      .center()
+      .colored("text" + type, colorPlay))
+    .on(this.object)
+    .at(0, 0.7, -1.3)
+    .obj();
+  this.btns = [];
+  this.btnState = [];
+  this.type = type;
   range(numButtons, function(i){
-    var btn = env.createElement("button");
+    var x = Math.floor(i / height),
+      y = i % height,
+      lon = (midWidth - x) * 10,
+      lat = (y - midHeight) * 10,
+      btn = box(padSize, padSize, padDepth)
+        .colored("btn" + i, colorOff)
+        .latLon(lat, lon);
+    put(box(padSize * 1.1, padSize * 1.1, padDepth * 0.9)
+        .colored("bevel" + i, colorPlay))
+      .on(btn);
+    env.registerPickableObject(btn);
+    btn.onselect = this.select.bind(this, i);
+    btn.onenter = this.play.bind(this, i);
+    this.object.add(btn);
+    this.btns.push(btn);
+  }.bind(this));
+}
 
-    // We can wire up event handlers on the button just like it was
-    // a DOM element.
-    btn.addEventListener("click", playNote.bind(null, i));
+Board.prototype.play = function(i) {
+  this.highlight(i, colorPlay);
+  this.object.getWorldDirection(DIST);
+  env.music.play(this.type, 25 - numButtons + i * 4, 0.25, perMeasure * 0.85,
+    this.object.x, this.object.y, this.object.z,
+    DIST.x, DIST.y, DIST.z)
+    .then(function(){
+      this.highlight(i);
+    }.bind(this));
+}
 
-    // Spacing the buttons out in a rough arc around the origin.
-    var x = (i - middle) * 0.25,
-      z = -1.5 * Math.cos(x) + 1,
-      // Put the element into the scene
-      btnBase = env.appendChild(btn);
-    btnBase.position.set(x, 0, z);
+Board.prototype.update = function() {
+  if(measure !== lastMeasure){
+    for(var y = 0; y < height; ++y){
+      var i = y * width + measure;
+      if(this.btnState[i]){
+        this.play(i);
+      }
+    }
+  }
+  DIST.set(this.object.position.x, env.input.head.position.y, this.object.position.z);
+  this.object.position.lerp(DIST, 0.01);
+};
+
+Board.prototype.highlight = function(i, color) {
+  color = color || this.btnState[i] && colorOn || colorOff;
+  var btn = this.btns[i]
+  btn.colored(btn.name, color);
+};
+
+Board.prototype.select = function(i, evt) {
+  if(!env.input.hasMouse || evt.pointer.name === "MousePointer") {
+    this.btnState[i] = !this.btnState[i];
+    this.highlight(i);
+  }
+};
+
+env.addEventListener("ready", function () {
+  const types = Primrose.Output.Music.TYPES,
+    nTypes = types.length;
+  types.forEach(function(type, t) {
+    var board = new Board(type);
+    boards.push(board);
+    env.scene.add(board.object);
+    board.object.latLon(0, (t - (nTypes - 1) / 2) * 100 / nTypes);
   });
+});
+
+env.addEventListener("update", function(dt){
+  t += dt;
+  if(t > perMeasure){
+    t -= perMeasure;
+    measure = (measure + 1) % width;
+  }
+  boards.forEach(function(board) {
+    board.update();
+  });
+  lastMeasure = measure;
 });
