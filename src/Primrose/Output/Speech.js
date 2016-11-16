18b3c41cebccbbb38b866f7da5b1f411f62e6ed7
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-11-15 22:48:58 -0500

    clean up speech synthesis

--- src/Primrose/Output/Speech.js
+++ src/Primrose/Output/Speech.js
@@ -1,10 +1,3 @@
-function wrap(thunk) {
-  return function(evt) {
-    this.speaking = false;
-    thunk(evt);
-  };
-}
-
 pliny.class({
   parent: "Primrose.Output",
     name: "Speech",
@@ -15,29 +8,39 @@ const Speech = (function(){
     return class {
       constructor (options) {
         this.options = patch(options, {
-          remoteVoices: false,
+          remoteVoices: true,
           volume: 1,
-          rate: 2,
-          pitch: 2,
+          rate: 1,
+          pitch: 1,
           voice: 0
         });
-        this.voices = speechSynthesis
-          .getVoices()
-          .filter((v) => this.options.remoteVoices || v.default || v.localService);
-        this.speaking = false;
+
+        const getVoices = () => {
+          this.voices = speechSynthesis
+            .getVoices()
+            .filter((v) => this.options.remoteVoices || v.default || v.localService);
+          this.voiceNames = this.voices.map((voice) => voice.name);
+        };
+
+        getVoices();
+        speechSynthesis.onvoiceschanged = getVoices;
+      }
+
+      get speaking(){
+        return speechSynthesis.speaking;
       }
 
       speak(txt, opts) {
         return new Promise((resolve, reject) => {
-          this.speaking = true;
           var msg = new SpeechSynthesisUtterance();
           msg.voice = this.voices[opts && opts.voice || this.options.voice];
           msg.volume = opts && opts.volume || this.options.volume;
           msg.rate = opts && opts.rate || this.options.rate;
           msg.pitch = opts && opts.pitch || this.options.pitch;
           msg.text = txt;
-          msg.onend = wrap(resolve).bind(this);
-          msg.onerror = wrap(reject).bind(this);
+          msg.onend = resolve;
+          msg.onerror = reject;
+          msg.onboundary = console.log.bind(console, "boundary");
           speechSynthesis.speak(msg);
         });
       }
