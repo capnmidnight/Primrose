0235f363e1c9bb6f01e6de0abbb312e40c6ff0fb
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-11-28 15:53:05 -0800

    simplify music demo to what people would more likely do

--- doc/music/app.js
+++ doc/music/app.js
@@ -1,50 +1,61 @@
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (factory());
-}(this, (function () { 'use strict';
+var height = 8,
+    width = 4,
+    numButtons = width * height,
+    midWidth = (width - 1) / 2,
+    midHeight = (height - 1) / 2,
+    colorOn = 0xffffff,
+    colorOff = 0xff0000,
+    colorPlay = 0xffff00,
+    boards = [],
+    t = 0,
+    measure = 0,
+    lastMeasure = null,
+    padSize = 0.22,
+    padDepth = 0.02,
+    textSize = 0.05,
+    skipOne = false,
+    perMeasure = 2 / height,
+    DIST = v3(),
+    env = new Primrose.BrowserEnvironment({
+      groundTexture: "https://www.primrosevr.com/doc/images/deck.png",
+      font: "https://primrosevr.com/doc/fonts/helvetiker_regular.typeface.json",
+      backgroundColor: 0x000000,
+      useFog: true,
+      useGaze: isMobile,
+      drawDistance: 10,
+      fullScreenButtonContainer: "body"
+    });
 
-var height = 8;
-var width = 4;
-var numButtons = width * height;
-var midWidth = (width - 1) / 2;
-var midHeight = (height - 1) / 2;
-var colorOn = 0xffffff;
-var colorOff = 0xff0000;
-var colorPlay = 0xffff00;
-var boards = [];
-var t = 0;
-var measure = 0;
-var lastMeasure = null;
-var padSize = 0.22;
-var padDepth = 0.02;
-var textSize = 0.05;
-var skipOne = false;
-var perMeasure = 2 / height;
-var DIST = v3();
-var env = new Primrose.BrowserEnvironment({
-  groundTexture: "https://www.primrosevr.com/doc/images/deck.png",
-  font: "https://primrosevr.com/doc/fonts/helvetiker_regular.typeface.json",
-  backgroundColor: 0x000000,
-  useFog: true,
-  useGaze: isMobile,
-  drawDistance: 10,
-  fullScreenButtonContainer: "body"
-});
+function text(size, text) {
+  size = size || 1;
+  return text3D(size * textSize, text).center();
+}
 
-function Board(type) {
+function Board(type){
   this.object = hub();
-  this.title = put(text3D(0.15, type).center().colored(colorPlay).named("text" + type)).on(this.object).at(0, 0.7, -1.3).obj();
+  this.title = put(text3D(0.15, type)
+      .center()
+      .colored(colorPlay)
+      .named("text" + type))
+    .on(this.object)
+    .at(0, 0.7, -1.3)
+    .obj();
   this.btns = [];
   this.btnState = [];
   this.type = type;
-  range(numButtons, function (i) {
+  range(numButtons, function(i){
     var x = Math.floor(i / height),
-        y = i % height,
-        lon = (midWidth - x) * 10,
-        lat = (y - midHeight) * 10,
-        btn = box(padSize, padSize, padDepth).colored(colorOff).named("btn" + i).latLon(lat, lon);
-    put(box(padSize * 1.1, padSize * 1.1, padDepth * 0.9).colored(colorPlay).named("bevel" + i)).on(btn);
+      y = i % height,
+      lon = (midWidth - x) * 10,
+      lat = (y - midHeight) * 10,
+      btn = box(padSize, padSize, padDepth)
+        .colored(colorOff)
+        .named("btn" + i)
+        .latLon(lat, lon);
+    put(box(padSize * 1.1, padSize * 1.1, padDepth * 0.9)
+        .colored(colorPlay)
+        .named("bevel" + i))
+      .on(btn);
     env.registerPickableObject(btn);
     btn.onselect = this.select.bind(this, i);
     btn.onenter = this.play.bind(this, i, 0);
@@ -53,22 +64,24 @@ function Board(type) {
   }.bind(this));
 }
 
-Board.prototype.play = function (i, dt) {
+Board.prototype.play = function(i, dt) {
   this.highlight(i, colorPlay);
   this.object.getWorldDirection(DIST);
   var duration = perMeasure * 0.85;
-  env.music.play(this.type, 25 - numButtons + i * 3, 0.25, duration, dt).at(this.object.x, this.object.y, this.object.z, DIST.x, DIST.y, DIST.z);
+  env.music.play(this.type, 25 - numButtons + i * 3, 0.25, duration, dt)
+    .at(this.object.x, this.object.y, this.object.z,
+        DIST.x, DIST.y, DIST.z);
   setTimeout(this.highlight.bind(this, i), duration * 1000);
-};
+}
 
-Board.prototype.update = function () {
-  if (measure !== lastMeasure) {
-    var time = env.audio.context.currentTime,
-        measureTime = perMeasure * Math.ceil(time / perMeasure),
-        dt = measureTime - time;
-    for (var y = 0; y < width; ++y) {
+Board.prototype.update = function() {
+  if(measure !== lastMeasure){
+    const time = env.audio.context.currentTime,
+          measureTime = perMeasure * Math.ceil(time / perMeasure),
+          dt = measureTime - time;
+    for(var y = 0; y < width; ++y){
       var i = y * height + measure;
-      if (this.btnState[i]) {
+      if(this.btnState[i]){
         this.play(i, dt);
       }
     }
@@ -77,23 +90,24 @@ Board.prototype.update = function () {
   this.object.position.lerp(DIST, 0.01);
 };
 
-Board.prototype.highlight = function (i, color) {
+
+Board.prototype.highlight = function(i, color) {
   color = color || this.btnState[i] && colorOn || colorOff;
-  var btn = this.btns[i];
+  var btn = this.btns[i]
   btn.colored(color).named(btn.name);
 };
 
-Board.prototype.select = function (i, evt) {
-  if (!env.input.hasMouse || evt.pointer.name === "MousePointer") {
+Board.prototype.select = function(i, evt) {
+  if(!env.input.hasMouse || evt.pointer.name === "MousePointer") {
     this.btnState[i] = !this.btnState[i];
     this.highlight(i);
   }
 };
 
 env.addEventListener("ready", function () {
-  var types = Primrose.Audio.Music.TYPES,
-      nTypes = types.length;
-  types.forEach(function (type, t) {
+  const types = Primrose.Audio.Music.TYPES,
+    nTypes = types.length;
+  types.forEach(function(type, t) {
     var board = new Board(type);
     boards.push(board);
     env.scene.add(board.object);
@@ -101,23 +115,17 @@ env.addEventListener("ready", function () {
   });
 });
 
-env.addEventListener("update", function (dt) {
-  if (!skipOne) {
+env.addEventListener("update", function(dt){
+  if(!skipOne){
     t += dt;
-    if (t > perMeasure) {
+    if(t > perMeasure){
       t -= perMeasure;
       measure = (measure + 1) % height;
     }
-    boards.forEach(function (board) {
-      return board.update();
-    });
+    boards.forEach((board) => board.update());
     lastMeasure = measure;
   }
   skipOne = false;
 });
 
-window.addEventListener("focus", function () {
-  return skipOne = true;
-});
-
-})));
+window.addEventListener("focus", () =>  skipOne = true);
