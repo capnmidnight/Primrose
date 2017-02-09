f36b42d819aafb6ed880a2db9358ee4731fb09d2
Sean T. McBeth <sean.mcbeth@gmail.com>
2017-02-09 00:42:24 -0500

    shadows in forest demo

--- demos/forest/app.js
+++ demos/forest/app.js
@@ -1,8 +1,9 @@
 var env = new Primrose.BrowserEnvironment({
   font: "../shared_assets/fonts/helvetiker_regular.typeface.json",
-  backgroundColor: 0x07001f,
+  backgroundColor: 0xC9E6EE,
   groundTexture: "../shared_assets/images/grass.png",
   useFog: true,
+  enableShadows: true,
   fullScreenButtonContainer: "#fullScreenButtonContainer",
   progress: Preloader.thunk
 });
@@ -11,6 +12,8 @@ var treeReady = Primrose.Graphics.ModelFactory.loadModel("tree.obj");
 
 env.addEventListener("ready", function() {
   treeReady.then(function(treeModel) {
+    treeModel.template.castShadow = true;
+    treeModel.template.receiveShadow = true;
     for(var i = 0; i < 100; ++i) {
       treeModel.clone()
         .named("tree" + i)
@@ -18,7 +21,15 @@ env.addEventListener("ready", function() {
         .at(
           Primrose.Random.number(-25, 25),
           0,
-          Primrose.Random.number(-25, 25));
+          Primrose.Random.number(-25, 25))
+        .rot(
+          0,
+          Primrose.Random.number(0, Math.PI),
+          0)
+        .scl(
+          1,
+          Primrose.Random.number(0.7, 1.3),
+          1);
     }
     Preloader.hide();
   });
