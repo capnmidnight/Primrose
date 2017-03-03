2b788cdf4c844d4f4cc8adaa5bc748f3d7a63f6b
Sean T. McBeth <sean.mcbeth@gmail.com>
2017-03-03 01:40:47 -0500

    move neck modeling out of BrowserEnvironment to CardboardVRDisplay

--- src/Primrose/Input/VR.js
+++ src/Primrose/Input/VR.js
@@ -52,6 +52,18 @@ export default class VR extends PoseInputProcessor {
     this.lastStageDepth = null;
     this.isStereo = false;
     installPolyfills(options);
+
+
+    if(this.options.nonstandardIPD !== null){
+      CardboardVRDisplay.IPD = this.options.nonstandardIPD;
+    }
+    if(this.options.nonstandardNeckLength !== null){
+      CardboardVRDisplay.NECK_LENGTH = this.options.nonstandardNeckLength;
+    }
+    if(this.options.nonstandardNeckDepth !== null){
+      CardboardVRDisplay.NECK_DEPTH = this.options.nonstandardNeckDepth;
+    }
+
     this.ready = navigator.getVRDisplays()
       .then((displays) => {
         this.displays.push.apply(this.displays, displays);
