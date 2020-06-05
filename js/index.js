eafea0f96ff8bb9a4d3bfe3a6515658af2771d20
Sean T. McBeth <sean.mcbeth+gh@gmail.com>
2020-06-05 01:51:10 -0400

    theme and grammar options added to demo

--- js/index.js
+++ js/index.js
@@ -1,14 +1,29 @@
-﻿import { Primrose } from "./package/primrose.min.js";
+﻿import { Primrose, grammars, themes } from "./package/primrose.min.js";
 
 (async function() {
     await Primrose.ready
-    const response = await fetch("test.js"),
-        code = await response.text();
 
-    for (let editor of Primrose.editors) {
-        editor.scaleFactor = devicePixelRatio;
-        editor.value = code;
-    }
+    const demosSel = document.querySelector("#demos"),
+        themesSel = document.querySelector("#themes"),
+        loadDemo = async () => {
+
+        const file = "test." + demosSel.value,
+            lang = grammars.get(demosSel.value),
+            theme = themes.get(themesSel.value),
+            response = await fetch(file),
+            code = await response.text();
+
+        for (let editor of Primrose.editors) {
+            editor.scaleFactor = devicePixelRatio;
+            editor.value = code;
+            editor.language = lang;
+            editor.theme = theme;
+        }
+    };
+
+    demosSel.addEventListener("input", loadDemo);
+    themesSel.addEventListener("input", loadDemo);
+    loadDemo();
 })();
 
 window.addEventListener("wheel", (evt) => {
